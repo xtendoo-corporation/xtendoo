3536d36bb90ea4ae0b856da9878e4d7e446b09e4
dani-ddl <dani--ef-@hotmail.com>
2019-12-10 11:38:50 +0100

    añadido modulo purchase_order_cost_price

---
+++ purchase_order_cost_price/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Purchase Order Cost Price",
+    "summary": "If the relationship with the supplier does not exist or its price is 0.00, use the cost price",
+    "version": "12.0.1.0.0",
+    "category": "Purchases",
+    "website": "",
+    "author": "ddl",
+    "license": "AGPL-3",
+    "depends": [
+        "purchase",
+    ],
+    "data": [
+    ],
+}
---
+++ purchase_order_cost_price/models/purchase.py
@@ -0,0 +1,52 @@
+# -- coding: utf-8 --
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models, _
+from odoo.tools.float_utils import float_compare
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+from odoo import api, fields, models, SUPERUSER_ID, _
+from odoo.osv import expression
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+from odoo.tools.float_utils import float_compare
+from odoo.exceptions import UserError, AccessError
+from odoo.tools.misc import formatLang
+from odoo.addons import decimal_precision as dp
+
+import logging
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    @api.onchange('product_id')
+    def onchange_product_id(self):
+        result = {}
+        if not self.product_id:
+            return result
+
+        # Reset date, price and quantity since _onchange_quantity will provide default values
+        self.date_planned = datetime.today().strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+
+        #ddl
+        self.price_unit = self.product_id.standard_price
+        #ddl
+
+        self.product_uom = self.product_id.uom_po_id or self.product_id.uom_id
+        result['domain'] = {'product_uom': [('category_id', '=', self.product_id.uom_id.category_id.id)]}
+
+        product_lang = self.product_id.with_context(
+            lang=self.partner_id.lang,
+            partner_id=self.partner_id.id,
+        )
+        self.name = product_lang.display_name
+        if product_lang.description_purchase:
+            self.name += '\n' + product_lang.description_purchase
+
+        self._compute_tax_id()
+
+        self._suggest_quantity()
+        self._onchange_quantity()
+
+        return result
