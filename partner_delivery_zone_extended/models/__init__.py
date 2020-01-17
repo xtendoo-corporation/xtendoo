e68a593f30754eedb0c35e5d8d769c1794385cf5
manuelcalerosolis <mcalero@gestool.es>
2020-01-17 10:51:15 +0100

    sale tier validation

--- partner_delivery_zone_extended/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
-from . import partner_delivery_zone
-from . import partner_delivery_zone_visit
-from . import res_partner
-from . import sale_order
-from . import stock_move
-from . import stock_picking
