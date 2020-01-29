34ae0c85b3616221567687c3af9ebac989303bb0
dani-ddl <dani--ef-@hotmail.com>
2020-01-29 12:47:04 +0100

    añadido sale_order_general_discount_and_dpp

---
+++ sale_order_general_discount_and_dpp/models/res_partner.py
@@ -0,0 +1,18 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+from odoo.addons import decimal_precision as dp
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    client_discount = fields.Float(
+        digits=dp.get_precision('Discount'),
+        string='Discount (%)'
+    )
+
+    discount_dpp = fields.Float(
+        digits=dp.get_precision('Discount'),
+        string='Discount DPP (%)'
+    )
