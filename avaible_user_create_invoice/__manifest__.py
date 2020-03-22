3329475ecf4e8cceb537a2bb9d1db29d15799d7f
Manuel Calero <manuel.calero@tecnativa.com>
2020-03-22 20:03:37 +0100

    Locked date payment

--- avaible_user_create_invoice/__manifest__.py
+++ avaible_user_create_invoice/__manifest__.py
@@ -1,7 +1,7 @@
 {
-    'name': 'avaible_user_create_invoice',
-    'summary': """Avaible User Create Invoice""",
-    'version': '12.0.1.0.0',
+    'name': 'Avaible User Create Invoice',
+    'summary': """Avaible User Create Invoice and modify payment date""",
+    'version': '12.0.1.0.0', 
     'description': """Avaible User create Invoice""",
     'author': 'DDL',
     'company': 'Xtendoo',
@@ -13,6 +13,7 @@
     'license': 'AGPL-3',
     'data': [
         'views/view_users_form_create_invoice.xml',
+        'views/account_payment.xml',
     ],
     "depends": [
         'sale',
--- avaible_user_create_invoice/models/__init__.py
+++ avaible_user_create_invoice/models/__init__.py
@@ -1,2 +1,3 @@
 from . import user
 from . import account_invoice
+from . import account_payment
