0e731ea298174a8acfbf98ab6a971a3abae0ec8f
Manuel Calero <manuel.calero@tecnativa.com>
2020-04-26 13:25:40 +0200

    stock barcodes internal transfer

---
+++ stock_barcodes_internal_transfer/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2020 Manuel Calero - Xtendoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Barcodes Internal Transfer",
+    "summary": "It provides read barcode for internal transfers.",
+    "version": "12.0.1.0.0",
+    "author": "Tecnativa, "
+              "Odoo Community Association (OCA)",
+    "website": "https://odoo-community.org/",
+    "license": "AGPL-3",
+    "category": "Extra Tools",
+    "depends": [
+        'stock_barcodes',
+    ],
+    'installable': True,
+}
