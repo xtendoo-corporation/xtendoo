9de1f4eaf1e95c2c0be03dc3477c406f82f93e68
Manuel Calero <manuel.calero@tecnativa.com>
2020-04-30 19:18:08 +0200

    sale_tier_validation deleted from xtendo

--- sale_tier_validation/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright 2019 Open Source Integrators
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
-from . import sale_order
-from . import tier_definition
-from . import tier_validation
