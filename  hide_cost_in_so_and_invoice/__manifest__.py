d5cd0913a35f22e477acd3157fcc682b2850817b
dani-ddl <dani--ef-@hotmail.com>
2020-05-14 10:50:14 +0200

    añadido hide_cost_in_so_and_invoice

---
+++  hide_cost_in_so_and_invoice/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'hide_cost_in_so_and_invoice',
+    'summary': """Hide purchase_price in sale orders and invoices """,
+    'version': '12.0.1.0.0',
+    'description': """Hide purchase_price in sale orders and invoices""",
+    'author': 'DDL',
+    'company': 'Xtendoo',
+    'website': 'http://www.xtendoo.com',
+    'category': 'Extra Tools',
+    'depends': [
+        'base',
+        'account_invoice_margin',
+        'sale_margin'
+    ],
+    'license': 'AGPL-3',
+    'data': [
+        'views/account_invoice_views.xml',
+        'views/sale_order_views.xml'
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+
+}
