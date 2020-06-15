c9ea52adbabe16d8998360e400367dcb34ee4b7e
dani-ddl <dani--ef-@hotmail.com>
2020-06-15 11:01:39 +0200

    document_format_dis

--- document_format_dis/__manifest__.py
+++ document_format_dis/__manifest__.py
@@ -2,9 +2,9 @@
 
 {
     'name': 'document_format_dis',
-    'summary': """Formatos de documentos Discafe""",
+    'summary': """Formatos de documentos DIS""",
     'version': '12.0.1.0.0',
-    'description': """Formatos de documentos Discafe""",
+    'description': """Formatos de documentos DIS""",
     'author': 'DDL',
     'company': 'Xtendoo',
     'website': 'http://www.xtendoo.es',
@@ -18,25 +18,51 @@
     ],
     'license': 'AGPL-3',
     'data': [
-        'views/report_saleorder_document.xml',
-        'views/external_layout_clean.xml',
-        'views/report_saleorder_bluetooth.xml',
-        'views/report_delivery_document.xml',
-        'views/report_delivery_document_bluetooth.xml',
-        'views/report_invoice_document.xml',
-        'views/report_invoice_bluetooth.xml',
-        'views/saleorder_promotions.xml',
-        'views/report_saleorder_document_without_promotions.xml',
-        'views/report_saleorder_document_promotions.xml',
-        'views/invoice_promotions.xml',
-        'views/report_invoice_document_promotions.xml',
-        'views/report_invoice_document_without_promotions.xml',
-        'views/delivery_promotions.xml',
-        'views/report_delivery_document_promotions.xml',
-        'views/report_delivery_document_without_promotions.xml',
-        'views/report_payment_receipt_bluetooth.xml',
-        'views/stock_picking_report.xml',
-	'views/report_payment_receipt.xml'
+
+        #Cabecera y Pie
+        'views/layout/external_layout_clean.xml',
+
+        #Ventas
+        'views/sale/report_saleorder_document_without_promotions.xml',
+        'views/sale/report_saleorder_document_promotions.xml',
+        'views/sale/saleorder_promotions.xml',
+
+        #Ventas Bluetooth
+        'views/sale_bluetooth/report_saleorder_bluetooth_without_promotions.xml',
+        'views/sale_bluetooth/report_saleorder_bluetooth_with_promotions.xml',
+        'views/sale_bluetooth/saleorder_promotions_bluetooth.xml',
+
+        #Albarán
+        'views/delivery/report_delivery_document_promotions.xml',
+        'views/delivery/report_delivery_document_without_promotions.xml',
+        'views/delivery/delivery_promotions.xml',
+
+        #Albarán Bluetooth
+        'views/delivery_bluetooth/report_delivery_document_with_promotions_bluetooth.xml',
+        'views/delivery_bluetooth/report_delivery_document_without_promotions_bluetooth.xml',
+        'views/delivery_bluetooth/delivery_promotions_bluetooth.xml',
+
+        #Factura
+        'views/invoice/report_invoice_document_promotions.xml',
+        'views/invoice/report_invoice_document_without_promotions.xml',
+        'views/invoice/invoice_promotions.xml',
+
+        #Factura Bluetooth
+        'views/invoice_bluetooth/report_invoice_document_with_promotions_bluetooth.xml',
+        'views/invoice_bluetooth/report_invoice_document_without_promotions_bluetooth.xml',
+        'views/invoice_bluetooth/invoice_promotions_bluetooth.xml',
+
+        #Pagos
+        'views/payment/report_payment_receipt.xml',
+
+        #Pagos Bluetooth
+        'views/payment_bluetooth/report_payment_receipt_bluetooth.xml',
+
+        #Informe de carga
+        'views/workload/stock_picking_report.xml'
+
+
+
     ],
     'demo': [],
     'installable': True,
