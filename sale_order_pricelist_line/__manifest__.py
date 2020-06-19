54ef4bb982e07c761b6267f98a6814168950ff97
dani-ddl <dani--ef-@hotmail.com>
2020-06-19 12:14:50 +0200

    sale order line pricelist

--- sale_order_pricelist_line/__manifest__.py
+++ sale_order_pricelist_line/__manifest__.py
@@ -6,7 +6,7 @@
     'website': 'https://www.xtendoo.es',
     'license': 'AGPL-3',
     'depends': [
-        'sale_stock',
+        'sale',
         ],
     'data': [
         'views/sale_order_view.xml',
