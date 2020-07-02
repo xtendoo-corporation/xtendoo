7ea7df6f31ff172c3aa51477470fe336db0bd1c8
dani-ddl <dani--ef-@hotmail.com>
2020-07-02 12:10:47 +0200

    sale_order_line_pricelist

--- sale_order_pricelist_line/models/sale_order_line.py
+++ sale_order_pricelist_line/models/sale_order_line.py
@@ -27,22 +27,23 @@ class SaleOrderLine(models.Model):
     def product_id_change(self):
         result=super().product_id_change()
         self.pricelist_id = self.order_id.pricelist_id.id
-
-
         logging.info(result.get('pricelist_id'))
         return result
 
-    @api.model_create_multi
-    def create(self, vals_list):
-        print("*"*40)
-        print(vals_list)
-        for vals in vals_list:
-            vals['pricelist_id'] = self.pricelist_id
-        print(vals_list)
-        print("*"*40)
-        lines=super().create(vals_list)
-        #Lines.append({'pricelist_id':self.pricelist_id})
-        return lines
+    # @api.model_create_multi
+    # def create(self, vals_list):
+    #     logging.info("*"*40)
+    #     logging.info(vals_list)
+    #     for vals in vals_list:
+    #         vals.update({'pricelist_id': self.order_id.pricelist_id})
+    #
+    #     logging.info("*"*40)
+    #     logging.info(self.order_id.pricelist_id)
+    #     logging.info(vals_list)
+    #     logging.info("*"*40)
+    #     lines=super().create(vals_list)
+    #     #Lines.append({'pricelist_id':self.pricelist_id})
+    #     return lines
 
 
 
