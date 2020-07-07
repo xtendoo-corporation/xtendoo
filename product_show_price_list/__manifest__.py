ff4cdec242aefc75d6e3a997c58dcfe94e1b8c96
dani-ddl <dani--ef-@hotmail.com>
2020-07-07 10:26:26 +0200

    product_show_pricelist

---
+++ product_show_price_list/__manifest__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+###################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2018-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Niyas Raphy(<https://www.cybrosys.com>)
+#
+#    This program is free software: you can modify
+#    it under the terms of the GNU Affero General Public License (AGPL) as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+###################################################################################
+
+{
+    'name': 'Product Show Price List',
+    'summary': """Product Show Price List""",
+    'version': '12.0.1.0.0',
+    'description': """Product Show Price List""",
+    'author': 'Manuel Calero Solís',
+    'company': 'Xtendoo',
+    'website': 'http://www.xtendoo.com',
+    'category': 'Extra Tools',
+    'depends': [
+        'base',
+        'product',
+    ],
+    'license': 'AGPL-3',
+    'data': [
+        'views/view.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
