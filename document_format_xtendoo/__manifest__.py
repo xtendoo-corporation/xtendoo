e10a9184cb51d95f8b34befbbcd9599547d71fc8
dani-ddl <dani--ef-@hotmail.com>
2020-07-14 10:45:41 +0200

    document_format_xtendoo

---
+++ document_format_xtendoo/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'document_format_xtendoo',
+    'summary': """Formatos de documentos Xtendoo""",
+    'version': '12.0.1.0.0',
+    'description': """Formatos de documentos Xtendoo""",
+    'author': 'DDL',
+    'company': 'Xtendoo',
+    'website': 'http://www.xtendoo.es',
+    'category': 'Extra Tools',
+    'depends': [
+        'base',
+        'account',
+        'sale',
+        'web',
+        'stock'
+    ],
+    'license': 'AGPL-3',
+    'data': [
+
+       'views/report_saleorder_document.xml'
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+
+}
