c2eb41181526f730723bc3d6061aeea6b43f0282
Dani <dani.xtendoo@gmail.com>
2020-07-28 13:41:52 +0200

    añadido stock_picking_product_label_with_barcode_128

---
+++ stock_picking_product_label_with_barcode_128/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright Xtendoo
+
+{
+    'name': 'Print Labels in Picking with Barcode 128',
+    'summary': "Print Product Labels in a Stock Picking with barcode 128",
+    'author': "dani-xtendoo",
+    'website': "https://xtendoo.es",
+    'category': 'Warehouse',
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'depends': [
+        'stock'
+        ],
+    'data': [
+        'reports/labels_print.xml',
+        'reports/template_labels.xml'
+
+    ],
+    'development_status': 'Beta',
+}
