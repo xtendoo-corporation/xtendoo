51301f2750a7fa59b51a5f2c807be9256fb6ca88
Dani <dani.xtendoo@gmail.com>
2020-07-28 14:12:44 +0200

    añadido stock_picking_product_label_with_barcode_128

--- stock_picking_product_label_with_barcode_128/__manifest__.py
+++ stock_picking_product_label_with_barcode_128/__manifest__.py
@@ -1,20 +1,18 @@
+# -*- coding: utf-8 -*-
 # Copyright Xtendoo
 
 {
-    'name': 'Print Labels in Picking with Barcode 128',
-    'summary': "Print Product Labels in a Stock Picking with barcode 128",
-    'author': "dani-xtendoo",
-    'website': "https://xtendoo.es",
-    'category': 'Warehouse',
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'depends': [
-        'stock'
-        ],
-    'data': [
-        'reports/labels_print.xml',
-        'reports/template_labels.xml'
-
+    "name": "Print Labels in Picking",
+    "summary": "Print Product Labels in a Stock Picking",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-reporting",
+    "category": "Warehouse",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["stock"],
+    "data": [
+        "reports/report_print_labels.xml",
+        "reports/report_print_labels_template.xml",
     ],
-    'development_status': 'Beta',
+    "installable": True,
 }
