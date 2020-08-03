9250dcac9e019e594734fa7302ca892dacf28781
Dani <dani.xtendoo@gmail.com>
2020-08-03 14:20:24 +0200

    document_format_zoopet add product_labels

--- document_format_zoopet/__manifest__.py
+++ document_format_zoopet/__manifest__.py
@@ -1,34 +1,32 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'document_format_Zoopet',
-    'summary': """Formatos de documentos Zoopet""",
-    'version': '12.0.1.0.0',
-    'description': """Formatos de documentos Zoopet""",
-    'author': 'DDL',
-    'company': 'Xtendoo',
-    'website': 'http://www.xtendoo.com',
-    'category': 'Extra Tools',
-    'depends': [
-        'base',
-        'account',
-        'sale',
-        'web',
-        'stock',
-        'product'
+    "name": "document_format_Zoopet",
+    "summary": """Formatos de documentos Zoopet""",
+    "version": "12.0.1.0.0",
+    "description": """Formatos de documentos Zoopet""",
+    "author": "DDL",
+    "company": "Xtendoo",
+    "website": "http://www.xtendoo.com",
+    "category": "Extra Tools",
+    "depends": ["base", "account", "sale", "web", "stock", "product"],
+    "license": "AGPL-3",
+    "data": [
+        # layout
+        "views/layout/external_layout_clean.xml",
+        # delivery
+        "views/delivery/report_delivery_document.xml",
+        "views/delivery/report_delivery_document_without_price.xml",
+        # sale_order
+        "views/sale/report_saleorder_document.xml",
+        "views/sale/report_sale_order_document_without_price.xml",
+        # Invoice
+        "views/invoice/report_invoice_document.xml",
+        # Product Label
+        "views/product_label/paper_format.xml",
+        "views/product_label/label_template.xml",
     ],
-    'license': 'AGPL-3',
-    'data': [
-        'views/report_delivery_document.xml',
-        'views/report_delivery_document_without_price.xml',
-        'views/external_layout_clean.xml',
-        'views/report_saleorder_document.xml',
-        'views/report_invoice_document.xml',
-        'views/report_simple_label.xml',
-        'views/report_sale_order_document_without_price.xml',
-    ],
-    'demo': [],
-    'installable': True,
-    'auto_install': False,
-
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
 }
