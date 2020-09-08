f0482e38960b78f715fb6501052e9a32bcfbe459
Dani <dani.xtendoo@gmail.com>
2020-09-08 11:10:59 +0200

    aññadido sale_order_lot_selection_xtendoo

---
+++ xtendoo_sale_order_lot_selection/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Sale Order Lot Selection xtendoo',
+    'version': '12.0.1.0.0',
+    'category': 'Sales Management',
+    'author': "DDL-Xtendoo",
+    'website': 'https://github.com/OCA/sale-workflow',
+    'license': 'AGPL-3',
+    'depends': ['sale_stock'],
+    'data': ['view/sale_view.xml'],
+    'installable': True,
+}
