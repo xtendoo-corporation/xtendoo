9cabea6a05df2456f38e7fe8cd296c4d6763087d
Dani <dani.xtendoo@gmail.com>
2020-09-11 10:06:47 +0200

    prohibir cancelar pagos d_hr_administration

--- d_hr_administration/models/account_payment.py
+++ d_hr_administration/models/account_payment.py
@@ -1,6 +1,6 @@
 # -- coding: utf-8 --
-from odoo import api, models, fields, _
-
+from odoo import api, models, fields
+from odoo.exceptions import ValidationError
 
 class AccountPayment(models.Model):
     _inherit = "account.payment"
@@ -13,5 +13,12 @@ class AccountPayment(models.Model):
 
     def _get_lock_date(self):
         return not self.env.user.administration
+
+    @api.multi
+    def cancel(self):
+        if not self.env.user.administration:
+            raise ValidationError(("No tiene permisos para cancelar Pagos"))
+        res = super(AccountPayment, self).cancel()
+        return res
 
 
