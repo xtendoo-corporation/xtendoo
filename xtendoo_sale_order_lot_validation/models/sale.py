0aea051f91597b7e75d4d7c0241341a4d1dda84d
Manuel Calero <manuelcalero.xtendoo@gmail.com>
2020-09-28 14:43:28 +0200

    sale order validation lot

--- xtendoo_sale_order_lot_validation/models/sale.py
+++ xtendoo_sale_order_lot_validation/models/sale.py
@@ -15,25 +15,21 @@ class SaleOrder(models.Model):
                         _('You can\'t store this line %s with empty lot') %
                         line.product_id.name
                         )
-
-                product_lot_qty = self._product_lot_qty(so, line)
+                product_lot_qty = 0
+                if so.warehouse_id and line.product_id:
+                    quants = self.env['stock.quant'].search([
+                        ('location_id', 'child_of', so.warehouse_id.lot_stock_id.id),
+                        ('product_id', '=', line.product_id.id),
+                        ('lot_id', '=', line.lot_id.id),
+                    ])
+                    if quants:
+                        product_lot_qty = sum(quants.mapped('quantity'))
                 if product_lot_qty < line.product_uom_qty:
                     raise UserError(
                         _('Not enough stock in lot %s, only %.2f for product : %s') %
                         (line.lot_id.name, product_lot_qty, line.product_id.name )
                     )
         return res
-
-    def _product_lot_qty(self, so, line):
-        if so.warehouse_id and line.product_id:
-            quants = self.env['stock.quant'].search([
-                ('location_id', 'child_of', so.warehouse_id.lot_stock_id.id),
-                ('product_id', '=', line.product_id.id),
-                ('lot_id', '=', line.lot_id.id),
-            ])
-            if quants:
-                return sum(quants.mapped('quantity'))
-        return 0
 
 
 class SaleOrderLine(models.Model):
