5d4bdc581405d13a94eb95564cb66870f0c193f2
Manuel Calero <manuelcalero.xtendoo@gmail.com>
2020-10-02 12:20:28 +0200

    new module remove restriction stock picking send mail

---
+++ remove_restriction_stock_picking_send_mail/README.rst
@@ -0,0 +1,24 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+==================================
+Remove Delivery Meyhod Restriction
+==================================
+
+This module allows the user send a delivery note
+regardless of having delivery method.
+
+Credits
+=======
+
+Contributors
+------------
+
+* Manuel Calero
+* Javier Lagares
+
+Maintainer
+----------
+
+https://github.com/xtendoo-corporation/xtendoo
---
+++ remove_restriction_stock_picking_send_mail/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Remove Restriction Stock Picking Seng Mail',
+    'version': '12.0.1.0.0',
+    'category': 'Sales Management',
+    'author': "Manuel Calero, Javier Lagares",
+    'website': 'https://github.com/xtendoo-corporation/xtendoo',
+    'license': 'AGPL-3',
+<<<<<<< HEAD:remove_restriction_stock_picking_send_mail/__manifest__.py
+    'depends': ['delivery'],
+    'data': [
+        'views/delivery_view.xml'
+=======
+    'depends': ['sale_stock'],
+    'data': [
+        'views/sale_order_line.xml',
+>>>>>>> 52f85c79fb38fe25f68caa4fdc6f9a0aba5fd6dc:sale_order_pricelist_auto_update/__manifest__.py
+    ],
+    'installable': True,
+}
