a9eeaa86c0bd9fab7d2a68df56bdcdf531f5322f
JavierLagares99 <6lvjavierlr@gmail.com>
2020-10-02 13:13:49 +0200

    Update manifest

--- remove_restriction_stock_picking_send_mail/__manifest__.py
+++ remove_restriction_stock_picking_send_mail/__manifest__.py
@@ -5,15 +5,11 @@
     'author': "Manuel Calero, Javier Lagares",
     'website': 'https://github.com/xtendoo-corporation/xtendoo',
     'license': 'AGPL-3',
-<<<<<<< HEAD:remove_restriction_stock_picking_send_mail/__manifest__.py
-    'depends': ['delivery'],
+    'depends': [
+        'delivery',
+    ],
     'data': [
-        'views/delivery_view.xml'
-=======
-    'depends': ['sale_stock'],
-    'data': [
-        'views/sale_order_line.xml',
->>>>>>> 52f85c79fb38fe25f68caa4fdc6f9a0aba5fd6dc:sale_order_pricelist_auto_update/__manifest__.py
+        'views/delivery_view.xml',
     ],
     'installable': True,
 }
