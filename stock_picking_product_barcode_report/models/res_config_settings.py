c47063ad81da580c4907882a503166c3c4384982
Manuel Calero <manuelcalero.xtendoo@gmail.com>
2020-10-19 23:10:50 +0200

    labes now show correct the wizard

--- stock_picking_product_barcode_report/models/res_config_settings.py
+++ stock_picking_product_barcode_report/models/res_config_settings.py
@@ -22,3 +22,22 @@ class Company(models.Model):
         [("gs1_128", "Display GS1_128 format for barcodes")],
         string="Method to choose the barcode formating",
     )
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    def action_barcode_print(self):
+        out_picking = self.picking_type_code == 'outgoing'
+        location = self.location_id if out_picking else self.location_dest_id
+        action = self.env.ref(
+            'stock_barcodes.action_stock_barcodes_read_picking').read()[0]
+        action['context'] = {
+            'default_location_id': location.id,
+            'default_partner_id': self.partner_id.id,
+            'default_picking_id': self.id,
+            'default_res_model_id':
+                self.env.ref('stock.model_stock_picking').id,
+            'default_res_id': self.id,
+            'default_picking_type_code': self.picking_type_code,
+        }
+        return action
