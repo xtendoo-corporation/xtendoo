3192231bf4ce62ce7d2496350121ca86113c2953
Manuel Calero <manuelcalero.xtendoo@gmail.com>
2020-10-27 11:23:23 +0100

    new module xtendoo web widget digital signed

--- web_digital_sign/models/users.py
+++
@@ -1,9 +0,0 @@
-# See LICENSE file for full copyright and licensing details.
-
-from odoo import models, fields
-
-
-class Users(models.Model):
-    _inherit = "res.users"
-
-    digital_signature = fields.Binary(string="Signature")
