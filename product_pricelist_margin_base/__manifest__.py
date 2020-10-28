7b32414aef04338f6111627fe0c5107cd1b43067
JavierLagares99 <6lvjavierlr@gmail.com>
2020-10-28 12:16:25 +0100

    product pricelist margin base update

---
+++ product_pricelist_margin_base/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Product Pricelist Margin Base',
+    'version': '13.0.1.0.0',
+    'category': 'Sales Management',
+    'author': "Manuel Calero, Javier Lagares",
+    'website': 'https://github.com/xtendoo-corporation/xtendoo',
+    'license': 'AGPL-3',
+    'depends': [
+        'product',
+    ],
+    'installable': True,
+}
