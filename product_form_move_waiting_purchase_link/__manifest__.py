18161d24d10bb084b9a53a844e0f6152e9c7ede8
Dani <dani.xtendoo@gmail.com>
2020-11-06 11:08:45 +0100

    añadido product_form_move_waiting_purchase_link

---
+++ product_form_move_waiting_purchase_link/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "product form move waiting purchase link",
+    "summary": """Añade un smart button al picking, para acceder a un listado de las cantidad pendientes de entregar de sus productos""",
+    "version": "13.0.1.0.0",
+    "description": """Añade un smart button al picking, para acceder a un listado de las cantidad pendientes de entregar de sus productos""",
+    "author": "DDL",
+    "company": "Xtendoo",
+    "website": "https://xtendoo.es",
+    "category": "Extra Tools",
+    "depends": ["sale",],
+    "license": "AGPL-3",
+    "data": [
+            'views/stock_picking.xml',
+            ]
+}
