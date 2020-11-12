74e0741bc95537bb43f20b4fcf080828f74eedc9
Manuel Calero <manuelcalero.xtendoo@gmail.com>
2020-11-13 00:24:38 +0100

    new columns tos stock move line

--- stock_picking_return_lot_qty/__manifest__.py
+++ stock_picking_return_lot_qty/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Tecnativa - Carlos Roca
+# Copyright 2020 Xtendoo - Manuel Calero
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
@@ -6,11 +6,11 @@
     "summary": "Return  to delivered quantity",
     "version": "13.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "author": "Xtendoo, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-workflow/",
     "depends": ["stock"],
     'data': [
         'wizard/stock_picking_return_views.xml',
+        'views/stock_move_line_views.xml',
     ],
-    "data": [],
 }
