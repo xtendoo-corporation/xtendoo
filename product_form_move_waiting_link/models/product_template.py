d0ab71bd3475d51baa2f6834a81ac344490f7858
Manuel Calero <manuelcalero.xtendoo@gmail.com>
2020-11-23 09:50:53 +0100

    select picking price

---
+++ product_form_move_waiting_link/models/product_template.py
@@ -0,0 +1,17 @@
+from odoo import api, fields, models
+from odoo.tools.float_utils import float_round
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    move_product_qty = fields.Float(
+        compute='_compute_move_waiting_product_qty',
+        string='Pickings'
+    )
+
+    def _compute_move_waiting_product_qty(self):
+        for template in self:
+            template.move_product_qty = float_round(
+                sum([p.move_product_qty for p in template.product_variant_ids]),
+                precision_rounding=template.uom_id.rounding)
--- stock_picking_update_price/wizards/select_picking_price.py
+++ stock_picking_update_price/wizards/select_picking_price.py
@@ -10,18 +10,26 @@ class SelectPickingPrice(models.Model):
     _name = 'select.picking.price'
     _description = 'Select Picking Price Wizard'
 
+    name = fields.Char(
+        string="Select Picking Price",
+        help="Name Select Picking Price",
+        required=True,
+        index=True,
+        track_visibility="always",
+        default=lambda self: self.env['ir.sequence'].next_by_code('stock.picking.update.price'),
+    )
     picking_id = fields.Many2one(
         'stock.picking',
-        'Stock Picking'
+        'Stock Picking',
     )
     price_line_ids = fields.One2many(
         'select.picking.price.line',
-        'select_picking_id'
+        'select_picking_id',
     )
     date = fields.Datetime(
         'Date',
         default=datetime.now(),
-        readonly=True
+        readonly=True,
     )
 
     @api.model
@@ -31,20 +39,29 @@ class SelectPickingPrice(models.Model):
             result['picking_id'] = self._context.get('default_picking_id')
         return result
 
+    # @api.onchange('picking_id')
+    # def _onchange_picking_id(self):
+    #     data = [(6, 0, [])]
+    #     product_pricelist_ids = self.env['product.pricelist'].search([('active', '=', True)])
+    #     for move_line in self.picking_id.move_line_ids:
+    #         data.append((0, False, self.get_list_price(move_line)))
+    #         for product_pricelist in product_pricelist_ids:
+    #             pricelist_item_ids = move_line.product_id.pricelist_item_ids.search(
+    #                 [('product_tmpl_id', '=', move_line.product_id.product_tmpl_id.id),
+    #                  ('pricelist_id', '=', product_pricelist.id)])
+    #             for pricelist_item in pricelist_item_ids:
+    #                 data.append((0, False, self.get_others_price(move_line, pricelist_item, product_pricelist)))
+    #     self.price_line_ids = data
+
     @api.onchange('picking_id')
     def _onchange_picking_id(self):
         data = [(6, 0, [])]
         product_pricelist_ids = self.env['product.pricelist'].search([('active', '=', True)])
-
         for move_line in self.picking_id.move_line_ids:
             data.append((0, False, self.get_list_price(move_line)))
             for product_pricelist in product_pricelist_ids:
-                pricelist_item_ids = move_line.product_id.pricelist_item_ids.search(
-                    [('product_tmpl_id', '=', move_line.product_id.product_tmpl_id.id),
-                     ('pricelist_id', '=', product_pricelist.id)])
-                for pricelist_item in pricelist_item_ids:
+                for pricelist_item in move_line.product_id.pricelist_item_ids:
                     data.append((0, False, self.get_others_price(move_line, pricelist_item, product_pricelist)))
-
         self.price_line_ids = data
 
     @staticmethod
@@ -88,20 +105,56 @@ class SelectPickingPriceLine(models.Model):
         default=True,
         help='Indicate this line is coming to change'
     )
-    select_picking_id = fields.Many2one('select.picking.price')
-    picking_id = fields.Many2one('stock.picking', required=True)
-    product_id = fields.Many2one('product.product', string='Product')
-    move_id = fields.Many2one('stock.move', string='Operations')
-    pricelist_id = fields.Many2one('product.pricelist')
-    list_price = fields.Float('List Price', digits=dp.get_precision('Product Price'))
-
-    product_text = fields.Text('Product Text', compute='_compute_price_line')
-    pricelist_text = fields.Text('Price List Text', compute='_compute_price_line')
-    cost_price = fields.Float('Cost Price', compute='_compute_price_line')
-    purchase_price = fields.Float('Purchase Price', compute='_compute_price_line')
-    margin = fields.Float('Margin', compute='_compute_price_line')
-    percent_margin = fields.Float('Percent Margin %', compute='_compute_price_line')
-    percent_sale_category = fields.Float('Sale Percent %', compute='_compute_price_line')
+    select_picking_id = fields.Many2one(
+        'select.picking.price'
+    )
+    picking_id = fields.Many2one(
+        'stock.picking',
+        required=True,
+    )
+    product_id = fields.Many2one(
+        'product.product',
+        string='Product',
+    )
+    move_id = fields.Many2one(
+        'stock.move',
+        string='Operations',
+    )
+    pricelist_id = fields.Many2one(
+        'product.pricelist',
+    )
+    list_price = fields.Float(
+        'List Price',
+        digits=dp.get_precision('Product Price'),
+    )
+    product_text = fields.Text(
+        'Product Text',
+        compute='_compute_price_line',
+    )
+    pricelist_text = fields.Text(
+        'Price List Text',
+        compute='_compute_price_line',
+    )
+    cost_price = fields.Float(
+        'Cost Price',
+        compute='_compute_price_line',
+    )
+    purchase_price = fields.Float(
+        'Purchase Price',
+        compute='_compute_price_line',
+    )
+    margin = fields.Float(
+        'Margin',
+        compute='_compute_price_line',
+    )
+    percent_margin = fields.Float(
+        'Percent Margin %',
+        compute='_compute_price_line',
+    )
+    percent_sale_category = fields.Float(
+        'Sale Percent %',
+        compute='_compute_price_line',
+    )
 
     @api.onchange('list_price')
     def _onchange_standard_price(self):
