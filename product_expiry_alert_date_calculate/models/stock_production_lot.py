c55cfb08391d1de3631368364bd243915f3c7761
Manuel Calero <manuelcalero.xtendoo@gmail.com>
2020-12-15 08:50:00 +0100

    clean dependencies

--- product_expiry_alert_date_calculate/models/stock_production_lot.py
+++ product_expiry_alert_date_calculate/models/stock_production_lot.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 
 from odoo import api, fields, models, _
-from datetime import date, timedelta
+from datetime import timedelta
 
 
 class StockProductionLot(models.Model):
