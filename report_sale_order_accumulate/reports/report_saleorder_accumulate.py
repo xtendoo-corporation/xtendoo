e02218390980560dab4a32f4e85cad9f2008cd65
Manuel Calero <manuelcalerosolis@gmail.com>
2021-01-04 14:31:30 +0100

    report sale order accumulate

---
+++ report_sale_order_accumulate/reports/report_saleorder_accumulate.py
@@ -0,0 +1,40 @@
+from odoo import api,fields, models, _
+
+
+class ReportSaleOrderLine(models.AbstractModel):
+    _name = 'report.report_sale_order_accumulate.report_sale_order_line'
+    _description = 'Report Quotations Products Accumulate'
+
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        return {
+            'docids': docids,
+            'get_sale_order_lines': self.get_sale_order_lines,
+        }
+
+    @api.model
+    def get_sale_order_lines(self, docids):
+        records = []
+        lines = self.env['sale.order.line'].read_group(
+            [('order_id', 'in', docids)],
+            ['product_id', 'product_uom_qty'],
+            ['product_id'],
+            lazy=False,
+        )
+        for line in lines:
+            product = self.env['product.product'].browse(line['product_id'][0])
+            records.append({'product_name': product.name,
+                            'product_classification_sequence': product.product_classification_id.sequence,
+                            'product_classification_name': product.product_classification_id.name,
+                            'product_qty': line['product_uom_qty']})
+        records.sort(key=lambda x: x['product_classification_sequence'])
+
+        for record in records:
+            print(record['product_name'])
+            print(record['product_classification_sequence'])
+            print(record['product_classification_name'])
+
+        return records
+
+
+
