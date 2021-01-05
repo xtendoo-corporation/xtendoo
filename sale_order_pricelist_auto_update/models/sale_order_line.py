17c5fa0c2216c09196f377c43a95dd8ea76e5db5
Manuel Calero <manuelcalerosolis@gmail.com>
2021-01-05 11:22:04 +0100

    price auto update

--- sale_order_pricelist_auto_update/models/sale_order_line.py
+++ sale_order_pricelist_auto_update/models/sale_order_line.py
@@ -15,8 +15,10 @@ class SaleOrderLine(models.Model):
     )
 
     @api.onchange('price_unit')
-    def _onchage_price_unit(self):
+    def _onchange_price_unit(self):
         if not self.product_id:
+            return
+        if self.product_id.standard_price == 0.0:
             return
         if self.price_unit < self.product_id.standard_price:
             raise UserError(
@@ -30,11 +32,18 @@ class SaleOrderLine(models.Model):
             'Payment Terms'
         )
         for line in self:
-            pricelist_price_unit = self.env['account.tax']._fix_tax_included_price_company(
-                line._get_display_price(line.product_id), line.product_id.taxes_id, line.tax_id, line.company_id
-            )
-            print("float_compare", float_compare(line.price_unit, pricelist_price_unit, precision_rounding=precision))
-            line.is_pricelist_change = ( (line.price_unit - pricelist_price_unit ) != 0.0 )
+            if line.product_id:
+                try:
+                    display_price = line._get_display_price(line.product_id)
+                    pricelist_price_unit = self.env['account.tax']._fix_tax_included_price_company(
+                        display_price, line.product_id.taxes_id, line.tax_id, line.company_id
+                    )
+                    print("float_compare", float_compare(line.price_unit, pricelist_price_unit, precision_rounding=precision))
+                    line.is_pricelist_change = ((line.price_unit - pricelist_price_unit) != 0.0)
+                except:
+                    line.is_pricelist_change = False
+            else:
+                line.is_pricelist_change = False
 
     def action_update_pricelist(self):
         print("action_update_pricelist*****************************************************")
