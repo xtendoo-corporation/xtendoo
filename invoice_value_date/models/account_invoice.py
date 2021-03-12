3a653b44db60d9c5977512ec4d67d679fbfcac21
Manuel Calero <manuelcalerosolis@gmail.com>
2021-03-12 11:47:26 +0100

    invoice value date

--- invoice_value_date/models/account_invoice.py
+++ invoice_value_date/models/account_invoice.py
@@ -14,5 +14,30 @@ class AccountInvoice(models.Model):
         string='Date Value',
         help="Keep empty to use the current date",
         copy=False,
-        default=_get_default_date,
     )
+
+    @api.model
+    def create(self, vals):
+        res = super(AccountInvoice, self).create(vals)
+        if vals['type'] == 'out_refund':
+            date = self._get_invoice_date(vals)
+        else:
+            date = self._get_sale_order_date(vals)
+        if date:
+            res.write({'date_value': date})
+        return res
+
+    def _get_sale_order_date(self, vals):
+        sale_order_date = None
+        for origin in vals['origin'].split(', '):
+            sale_order = self.env['sale.order'].search([('name', '=', origin)], limit=1)
+            if sale_order and (sale_order_date is None or sale_order < sale_order_date):
+                sale_order_date = sale_order.date_order
+        return sale_order_date
+
+    def _get_invoice_date(self, vals):
+        if vals['refund_invoice_id']:
+            invoice = self.env['account.invoice'].search([('id', '=', vals['refund_invoice_id'])], limit=1)
+            if invoice:
+                return invoice.date_invoice
+        return None
