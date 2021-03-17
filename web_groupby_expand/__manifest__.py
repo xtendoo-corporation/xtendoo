a507c8208af500866e18de7ca40a752a133848e2
Manuel Calero <manuelcalerosolis@gmail.com>
2021-03-17 12:39:15 +0100

    web groupby expand

---
+++ web_groupby_expand/__manifest__.py
@@ -0,0 +1,23 @@
+# See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "Web GroupBy Expand",
+    "version": "13.0.1.0.0",
+    "author": "Serpent Consulting Services Pvt. Ltd.",
+    "maintainer": "Serpent Consulting Services Pvt. Ltd.",
+    "website": "http://www.serpentcs.com",
+    "category": "Web",
+    "license": "AGPL-3",
+    'summary': 'Expand all groups on single click',
+    "depends": [
+        "web",
+    ],
+    "data": [
+        "views/templates.xml",
+    ],
+    "qweb": [
+        "static/src/xml/web_groups_expand.xml",
+    ],
+    "images": ["static/description/groupexpand.png"],
+    "installable": True,
+}
