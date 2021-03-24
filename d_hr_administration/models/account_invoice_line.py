def9e4c611a3c1a47ef46696e5256d5eb038fbff
dani <dani.xtendoo@gmail.com>
2021-03-24 13:13:23 +0100

    d_hr_administration actualizado

--- d_hr_administration/models/account_invoice_line.py
+++ d_hr_administration/models/account_invoice_line.py
@@ -7,20 +7,5 @@ import logging
 
 
 class AccountInvoiceLine(models.Model):
-    _inherit = 'account.invoice.line'
+    _inherit = ['account.invoice.line','administrator.mixin.rule']
     _name = 'account.invoice.line'
-
-    is_admin = fields.Boolean(
-        comodel_name='account.invoice.line',
-        compute='_is_admin',
-        string="isAdmin",
-        default=lambda self: self._get_default_admin()
-    )
-    @api.one
-    def _is_admin(self):
-        self.is_admin=self.env.user.administration
-        return
-
-    @api.model
-    def _get_default_admin(self):
-        return self.env.user.administration
