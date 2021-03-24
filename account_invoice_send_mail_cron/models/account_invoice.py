0e672eb52ed9757c07f1c4252b61ce6c40884d32
Manuel Calero <manuelcalerosolis@gmail.com>
2021-03-24 11:21:56 +0100

    new module account invoice send mail cron

--- account_invoice_send_mail_cron/models/account_invoice.py
+++ account_invoice_send_mail_cron/models/account_invoice.py
@@ -2,17 +2,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 from odoo import models, fields, api, _
-from odoo.exceptions import UserError, ValidationError
 
 
 class AccountInvoiceSendMailCron(models.Model):
     _name = "account.invoice.send.mail"
     _description = "Account Invoice Send Mail Cron"
 
+    @api.model
     def _cron_send_account_invoice_email(self):
         invoices = self.env['account.move'].search([
             ('invoice_sent', '=', False),
             ('type', '=', 'out_invoice'),
         ])
         print("invoices ::::", invoices)
-        raise ValidationError('Cron is running::::')
+
+
