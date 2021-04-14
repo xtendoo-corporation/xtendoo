7b3f191a2d7b20a814e9da2cde1eb553085a2d56
dani <dani.xtendoo@gmail.com>
2021-04-14 13:19:13 +0200

    Arreglo en boton de enregar y facturar

--- lch_administration/models/account_invoice.py
+++ lch_administration/models/account_invoice.py
@@ -16,13 +16,16 @@ class AccountInvoice(models.Model):
         """Si el contexto trae el dato 'active_model' y ese model es 'sale_order' eso quiere decir
         que viene de un pedido por tanto lo dejamos pasar
         """
-        #logging.info('get')
-        #logging.info(self.env.context.get('active_model', ''))
-        #logging.info(self.env.context.get('is_sale', ''))
-        if self.env.context.get('active_model', '') == 'sale.order' or self.env.user.administration or self.env.context.get('is_sale', '') == True:
-            return super(AccountInvoice, self).default_get(default_fields)
-
-        raise ValidationError(("You are not allowed to create invoices."))
+        if not self.env["res.users"].has_group("lch_administration.administration_group"):
+            active_model=""
+            if self._context.get('params'):
+                if self._context.get('params').get('model'):
+                    active_model=self._context.get('params').get('model')
+            if self._context.get('search_default_my_quotation'):
+                active_model="sale.order"
+            if active_model != "sale.order":
+                raise ValidationError(("No tiene permisos para crear facturas directas"))
+        return super(AccountInvoice, self).default_get(default_fields)
 
     @api.multi
     def action_invoice_cancel(self):
