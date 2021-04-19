c66fc43ccb8585760637aef73ea724964e4970e7
dani <dani.xtendoo@gmail.com>
2021-04-19 12:27:13 +0200

    Invoicing Monthly, tiene en cuenta el año

--- invoicing_monthly/models/sale_order.py
+++ invoicing_monthly/models/sale_order.py
@@ -11,23 +11,12 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     def _create_invoices(self, grouped=False, final=False):
-        #return super(SaleOrder, self)._create_invoices(self,grouped=False, final=False)
         for order in self:
             if order.partner_id.monthly_invoicing:
-                date_order=order.date_order
-                date_today=datetime.now()
-                if format(date_order.month) == format(date_today.month):
+                date_order=format(order.date_order.month)+format(order.date_order.year)
+                date_today=format(datetime.now().month)+format(datetime.now().year)
+                if date_order == date_today:
                     raise ValidationError(("El cliente %s factura mensualmente, el pedido %s tiene fecha del mes en curso") %(order.partner_id.name,order.name))
-        for order in self:
-            return super(SaleOrder, self)._create_invoices(self)
+        return super(SaleOrder, self)._create_invoices(self)
         
-
-
-    #@api.model
-    #def default_get(self, default_fields):
-        #print("*"*20)
-        #print(self._context)
-        #if self._context.get('default_type') == 'out_invoice':
-            #return super(AccountMove, self).default_get(default_fields)
-        #raise ValidationError(("No tiene permisos para crear facturas"))
-        #return super(AccountInvoice, self).default_get(default_fields)
+           
