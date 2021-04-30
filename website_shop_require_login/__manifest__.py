bd8cd6e9fbf92c6fc88b9c84ca1a9a9b43373e47
Manuel Calero <manuelcalerosolis@gmail.com>
2021-04-30 09:16:35 +0200

    website shop require login

---
+++ website_shop_require_login/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2021 Manuel Calero - Xtendoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Require login to shop",
+    "summary": "Force users to login for visit shop",
+    "version": "13.0.1.0.0",
+    "category": "Website",
+    "website": "https://github.com/OCA/e-commerce",
+    "author": "Xtendoo, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["website_sale_suggest_create_account"],
+}
