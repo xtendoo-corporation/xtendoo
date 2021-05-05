be4f537d2415c566ac261710f30baaa7047ad9ca
dani <dani.xtendoo@gmail.com>
2021-05-05 10:50:17 +0200

    add sale not cancel with validate pickings

--- sale_not_cancel_with_validate_picking/models/sale.py
+++ sale_not_cancel_with_validate_picking/models/sale.py
@@ -1,6 +1,7 @@
-# Copyright 2020 Manuel Calero Solis - Xtendoo
+# Copyright 2021 Daniel Domínguez- Xtendoo
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import _, api, fields, models
+from odoo.exceptions import AccessError, UserError, ValidationError
 
 
 class SaleOrder(models.Model):
@@ -8,5 +9,19 @@ class SaleOrder(models.Model):
 
     def action_cancel(self):
         res = super(SaleOrder, self).action_cancel()
-        print("pasaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+        picking_ids=self.env['stock.picking'].search([
+            ('sale_id', '=', self.id),
+            ('state', '=', 'done')
+        ])
+        picking_validates=""
+        if picking_ids:
+            for picking in picking_ids:
+                if picking_validates == '':
+                    picking_validates= picking.name
+                else:
+                    picking_validates=picking_validates+", "+picking.name
+            raise UserError(_("You cannot cancel an order with validated deliveries, you must first cancel %s") % (picking_validates))
+        self.env['stock.picking'].search([
+            ('sale_id', '=', self.id)
+        ]).unlink()
         return res
