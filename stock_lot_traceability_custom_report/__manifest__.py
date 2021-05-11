103627ea31217de73009de10388fc8cbd44db588
dani <dani.xtendoo@gmail.com>
2021-05-11 09:29:24 +0200

    añadido stock_lot_traceability_custom_report

---
+++ stock_lot_traceability_custom_report/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2020 Xtendoo -Daniel Domínguez
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Lot Traceability Custom Report",
+    "description": """
+    Modify the original traceability report
+    """,
+    "version": "13.0.1.3.6",
+    "category": "Stock",
+    "author": "Daniel Domínguez",
+    "website": "https://xtendoo.es/",
+    "license": "AGPL-3",
+    "depends": ["base", "stock",],
+    "data": ["views/report_stock_body_print.xml",],
+    "installable": True,
+}
