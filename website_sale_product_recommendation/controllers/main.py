2be2caae2fa2a2bfdecee56578d94abd94d59206
Manuel Calero <manuelcalerosolis@gmail.com>
2021-05-21 20:40:22 +0200

    sync

--- website_sale_product_recommendation/controllers/main.py
+++ website_sale_product_recommendation/controllers/main.py
@@ -18,40 +18,36 @@ class WebsiteSale(WebsiteSale):
             search, category, attrib_values, search_in_description=search_in_description
         )
         if "recommendations" in request.env.context:
-            print("*"*80)
-            print("domain recommendations", self._get_sale_order_domain())
-
-            print("*"*80)
             found_lines = request.env["sale.order.line"].read_group(
                 self._get_sale_order_domain(),
                 ["product_id", "qty_delivered"],
                 ["product_id"],
             )
-            print("found lines", found_lines)
-
-            for l in found_lines:
-                print("*" * 80)
-                print(l)
-
-            print("*"*80)
             products_ids = []
             for line in found_lines:
                 products_ids.append(line["product_id"][0])
-
-            print("*" * 80)
-            print("found_dict", products_ids)
-            print("*" * 80)
-
             domain.append(("id", "in", products_ids))
 
-        print("*" * 80)
-        print("domain", domain)
-        print("*" * 80)
+            print("*" * 80)
+            print("domain", domain)
+            print("*" * 80)
 
         return domain
 
-    @http.route(["/recommendations"], type="http", auth="public", website=True)
-    def shop(self, page=0, category=None, recommendations=True, search="", **post):
+    @http.route(
+        [
+            "/shop",
+            "/shop/page/<int:page>",
+            '/shop/category/<model("product.public.category"):category>',
+            '/shop/category/<model("product.public.category"):category'
+            ">/page/<int:page>",  # Continue previous line
+            "/shop/recommendations",
+        ],
+        type="http",
+        auth="public",
+        website=True,
+    )
+    def shop(self, page=0, category=None, recommendations=False, search="", **post):
         if recommendations:
             context = dict(request.env.context)
             context.setdefault("recommendations", recommendations)
@@ -59,15 +55,6 @@ class WebsiteSale(WebsiteSale):
         return super().shop(
             page=page, category=category, recommendations=recommendations, search=search, **post
         )
-
-    # Method to get the recommendations.
-    @http.route(["/page/product_recommendations"], type="http", auth="public", website=True)
-    def product_recommendations(self, **post):
-        keep = QueryURL("/page/product_recommendations")
-        values = {"keep": keep}
-        if post.get("search"):
-            values.update({"search": post.get("search")})
-        return request.render("website_sale_product_recommendation.product_recommendations", values)
 
     def _get_sale_order_domain(self):
         partner = request.env.user.partner_id
