096e5e1b1a21e8f80d98dfdb2ae90660844788ef
dani <dani.xtendoo@gmail.com>
2021-06-07 14:03:14 +0200

    añadido stock_picking_sale_order_pallets_and_lumps

---
+++ stock_picking_and_sale_order_pallets_and_lumps/models/stock_picking.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+import logging
+
+
+class StockPickingBatch(models.Model):
+    _inherit = ["stock.picking"]
+
+    lumps_number = fields.Integer(string="Lumps", store=True)
+
+    palets_number = fields.Integer(string="Palets", store=True)
