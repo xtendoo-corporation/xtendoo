bcf5e3b66bd3c39a56c942e228c4d6deb601c534
dani <dani.xtendoo@gmail.com>
2021-06-17 13:21:43 +0200

    Migration pricelist_constraint_unique to 13.0

---
+++ pricelist_constraint_unique/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020 Xtendoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Pricelist Contraint Unique",
+    "summary": """Pricelist Contraint Unique""",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "DDL-Xtendoo",
+    "website": "https://xtendoo.es",
+    "depends": ["base", "stock", "product",],
+    "data": [],
+    "installable": True,
+}
