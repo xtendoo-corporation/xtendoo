4cc0353e4603e8b7f48512b00e28c532adb9c3d8
dani <dani.xtendoo@gmail.com>
2021-06-17 13:40:57 +0200

    Migration document_format_pos to 13.0

---
+++ document_format_pos/__manifest__.py
@@ -0,0 +1,17 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Documento para el punto de venta",
+    "category": "Point Of Sale",
+    "author": "Xtendoo",
+    "website": "https://xtendoo.es",
+    "license": "AGPL-3",
+    "version": "13.0.2.0.0",
+    "depends": [
+        "point_of_sale",
+    ],
+    "qweb": [
+        "static/src/xml/pos.xml",
+    ],
+    "installable": True,
+}
