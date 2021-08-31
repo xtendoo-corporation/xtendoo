19f201649e76d7069934a16b0db93642cf79b737
dani <dani.xtendoo@gmail.com>
2021-08-31 11:42:45 +0200

    Refactor del modulo document_format_hr

--- document_format_hr/__manifest__.py
+++ document_format_hr/__manifest__.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+
+
+
+
+
 {
     'name': 'document_format_hr',
     'summary': """Formatos de documentos HR""",
@@ -22,31 +27,26 @@
         'views/layout/external_layout_clean.xml',
 
         #Ventas
-        #'views/sale/report_saleorder_document.xml',
         'views/sale/report_saleorder_document_without_promotions.xml',
         'views/sale/report_saleorder_document_promotions.xml',
         'views/sale/saleorder_promotions.xml',
 
         #Ventas Bluetooth
-        #'views/sale_bluetooth/report_saleorder_bluetooth.xml',
         'views/sale_bluetooth/report_saleorder_bluetooth_without_promotions.xml',
         'views/sale_bluetooth/report_saleorder_bluetooth_with_promotions.xml',
         'views/sale_bluetooth/saleorder_promotions_bluetooth.xml',
 
         #Albarán
-        #'views/delivery/report_delivery_document.xml',
         'views/delivery/report_delivery_document_promotions.xml',
         'views/delivery/report_delivery_document_without_promotions.xml',
         'views/delivery/delivery_promotions.xml',
 
         #Albarán Bluetooth
-        #'views/delivery_bluetooth/report_delivery_document_bluetooth.xml',
         'views/delivery_bluetooth/report_delivery_document_with_promotions_bluetooth.xml',
         'views/delivery_bluetooth/report_delivery_document_without_promotions_bluetooth.xml',
         'views/delivery_bluetooth/delivery_promotions_bluetooth.xml',
 
         #Factura
-        #'views/invoice/report_invoice_document.xml',
         'views/invoice/report_invoice_document_promotions.xml',
         'views/invoice/report_invoice_document_without_promotions.xml',
         'views/invoice/invoice_promotions.xml',
@@ -54,12 +54,7 @@
         #Factura sin promociones
         'views/invoice_without_promotions/invoice_without_promotions.xml',
 
-        # Factura descripcion
-        'views/invoice_with_description/invoice_with_description.xml',
-        'views/invoice_with_description/report_invoice_document_with_description.xml',
-
         #Factura Bluetooth
-        #'views/invoice_bluetooth/report_invoice_bluetooth.xml',
         'views/invoice_bluetooth/report_invoice_document_with_promotions_bluetooth.xml',
         'views/invoice_bluetooth/report_invoice_document_without_promotions_bluetooth.xml',
         'views/invoice_bluetooth/invoice_promotions_bluetooth.xml',
