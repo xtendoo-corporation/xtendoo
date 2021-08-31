57a38ac5cd0fd4849f98defaf27ce9614b8e97df
Manuel Calero <manuelcalerosolis@gmail.com>
2021-08-31 11:36:29 +0200

    error printing multiples invoices

--- stock_picking_and_sale_order_pallets_and_lumps/models/sale_order.py
+++ stock_picking_and_sale_order_pallets_and_lumps/models/sale_order.py
@@ -5,32 +5,32 @@ import logging
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    palets_number = fields.Integer(compute='compute_palets_number', string='Pallets number')
-
-    lumps_number = fields.Integer(compute='compute_lumps_number', string='Lumps number')
-
-    has_picking = fields.Boolean(compute='compute_has_picking', string='Has Picking')
+    palets_number = fields.Integer(
+        compute='compute_palets_number',
+        string='Pallets number',
+    )
+    lumps_number = fields.Integer(
+        compute='compute_lumps_number',
+        string='Lumps number',
+    )
+    has_picking = fields.Boolean(
+        compute='compute_has_picking',
+        string='Has Picking',
+    )
 
     def compute_palets_number(self):
-        palets_number=0
-        delivery_ids = self.env['stock.picking'].search([('sale_id', '=', self.id)])
-        for delivery_id in delivery_ids:
-            palets_number+=delivery_id.palets_number
-        self.palets_number=palets_number
+        for sale in self:
+            delivery_ids = self.env['stock.picking'].search([('sale_id', '=', sale.id)])
+            sale.palets_number = sum(delivery_ids.mapped('palets_number'))
 
     def compute_lumps_number(self):
-        lumps_number=0
-        delivery_ids = self.env['stock.picking'].search([('sale_id', '=', self.id)])
-        for delivery_id in delivery_ids:
-            lumps_number+=delivery_id.lumps_number
-        self.lumps_number=lumps_number
+        for sale in self:
+            delivery_ids = self.env['stock.picking'].search([('sale_id', '=', sale.id)])
+            sale.palets_number = sum(delivery_ids.mapped('lumps_number'))
 
     def compute_has_picking(self):
-        delivery_ids=self.env['stock.picking'].search([('sale_id', '=', self.id)])
-        has_picking=False
-        if delivery_ids:
-            has_picking=True
-        self.has_picking=has_picking
+        for sale in self:
+            self.has_picking = bool(self.env['stock.picking'].search([('sale_id', '=', sale.id)]))
 
 
 
--- stock_picking_and_sale_order_pallets_and_lumps/models/stock_picking.py
+++ stock_picking_and_sale_order_pallets_and_lumps/models/stock_picking.py
@@ -2,13 +2,16 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models, _
-from odoo.exceptions import UserError
-import logging
 
 
 class StockPickingBatch(models.Model):
     _inherit = ["stock.picking"]
 
-    lumps_number = fields.Integer(string="Lumps", store=True)
-
-    palets_number = fields.Integer(string="Palets", store=True)
+    lumps_number = fields.Integer(
+        string="Lumps",
+        store=True,
+    )
+    palets_number = fields.Integer(
+        string="Palets",
+        store=True,
+    )
