52920ba8e8569fb5d875ecca8525ec12bca0a94a
dani <dani.xtendoo@gmail.com>
2021-09-16 09:31:33 +0200

    stock_picking_batch_delivery, busqueda de la factura ahora con like

--- stock_picking_batch_delivery/models/stock_picking.py
+++ stock_picking_batch_delivery/models/stock_picking.py
@@ -11,7 +11,7 @@ class StockPickingBatch(models.Model):
             if picking.picking_type_id.id == 8:
                 if picking.origin != "":
                     invoice_id = self.env["account.invoice"].search(
-                        [("origin", "=", picking.origin)], limit=1
+                        [("origin", "like", picking.origin)]
                     )
                     picking.invoice_id = invoice_id
 
