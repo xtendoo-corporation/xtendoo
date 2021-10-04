14c1013d5781f115d39be53a37be7bc184e2cc04
Manuel Calero <manuelcalerosolis@gmail.com>
2021-10-04 18:39:44 +0200

    new module: sale_order_line_discount_view

---
+++ sale_order_line_discount_view/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2028 Xtendoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Sale Order Line Discount View",
+    "version": "13.0.1.1.0",
+    "summary": "Adds a discount to each sale order line.",
+    "author": "Xtendoo, Odoo Community Association (OCA)",
+    "website": "https://odoo-community.org/",
+    "category": "Sale",
+    "license": "AGPL-3",
+    "depends": ["sale_stock"],
+    "data": ["views/sale_order_view.xml", "reports/sale_order_report.xml"],
+}
