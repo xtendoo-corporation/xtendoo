674c8ec5e9a8b0598b6c276422eabe5726da0215
dani <dani.xtendoo@gmail.com>
2021-11-25 12:14:17 +0100

    [ADD] purchase_order_create_invoice v 12.0

---
+++ purchase_order_create_invoice/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 Manuel Calero - Xtendoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Purchase Order Create Invoice",
+    "version": "12.0.1.0.0",
+    "summary": "Generate single invoice from purchase orders",
+    "author": "Manuel Calero - Xtendoo",
+    "website": "https://xtendoo.es",
+    "category": "Sales",
+    "description": """
+        Generate single invoice from multiple purchase order
+    """,
+    "depends": ["sale", "sale_management", "purchase", "account"],
+    "data": [
+        "wizard/multiple_purchase_one_invoice_wizard.xml",
+        "views/sale_purchase_invoice.xml",
+    ],
+    "installable": True,
+}
