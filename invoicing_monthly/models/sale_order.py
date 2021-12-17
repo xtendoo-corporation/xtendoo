acc40f714af9e6f646fb003b488c6e06df17e865
Manuel Calero <manuelcalerosolis@gmail.com>
2021-12-17 20:31:06 +0100

    invoicing monthly error

--- invoicing_monthly/models/sale_order.py
+++ invoicing_monthly/models/sale_order.py
@@ -3,7 +3,6 @@
 
 from odoo import models, fields, api, _
 from odoo.exceptions import AccessError, UserError, ValidationError
-from itertools import groupby
 from datetime import datetime, timedelta
 
 
@@ -13,10 +12,9 @@ class SaleOrder(models.Model):
     def _create_invoices(self, grouped=False, final=False):
         for order in self:
             if order.partner_id.monthly_invoicing:
-                date_order=format(order.date_order.month)+format(order.date_order.year)
-                date_today=format(datetime.now().month)+format(datetime.now().year)
+                date_order = format(order.date_order.month)+format(order.date_order.year)
+                date_today = format(datetime.now().month)+format(datetime.now().year)
                 if date_order == date_today:
                     raise ValidationError(("El cliente %s factura mensualmente, el pedido %s tiene fecha del mes en curso") %(order.partner_id.name,order.name))
-        return super(SaleOrder, self)._create_invoices(self)
-        
-           
+        return super(SaleOrder, self)._create_invoices(grouped, final)
+
