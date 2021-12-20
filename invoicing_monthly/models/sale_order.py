5ac6fe52f5013544093daa00aa76a4c1699068fe
Manuel Calero <manuelcalerosolis@gmail.com>
2021-12-20 12:55:42 +0100

    invoicing monthly error

--- invoicing_monthly/models/sale_order.py
+++ invoicing_monthly/models/sale_order.py
@@ -12,9 +12,12 @@ class SaleOrder(models.Model):
     def _create_invoices(self, grouped=False, final=False):
         for order in self:
             if order.partner_id.monthly_invoicing:
-                date_order = format(order.date_order.month)+format(order.date_order.year)
-                date_today = format(datetime.now().month)+format(datetime.now().year)
+                date_order = format(order.date_order.month) + format(order.date_order.year)
+                date_today = format(datetime.now().month) + format(datetime.now().year)
                 if date_order == date_today:
-                    raise ValidationError(("El cliente %s factura mensualmente, el pedido %s tiene fecha del mes en curso") %(order.partner_id.name,order.name))
+                    raise ValidationError(
+                        ("El cliente %s factura mensualmente, el pedido %s tiene fecha del mes en curso")
+                        %
+                        (order.partner_id.name, order.name)
+                    )
         return super(SaleOrder, self)._create_invoices(grouped, final)
-
