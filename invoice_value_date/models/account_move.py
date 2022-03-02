2d9beb8fa3b801215e820f85aa106fc2a8300c44
dani <dani.xtendoo@gmail.com>
2022-03-02 10:31:53 +0100

    [ADD] invoice_value_date, arreglado bug que devolvia siempre False a la hora de insertar la fecha al crar la factura

--- invoice_value_date/models/account_move.py
+++ invoice_value_date/models/account_move.py
@@ -14,18 +14,21 @@ class AccountMove(models.Model):
         string='Date Value',
         help="Keep empty to use the current date",
         copy=False,
+        compute="_get_default_value_date",
+        store=True,
     )
 
     @api.model
     def create(self, vals):
-        res = super(AccountMove, self).create(vals)
-        if type in vals:
+        date = False
+        if vals['type']:
             if vals['type'] == 'out_refund':
                 date = self._get_invoice_date(vals)
             else:
                 date = self._get_sale_order_date(vals)
-            if date:
-                res.write({'date_value': date})
+        if date is not False:
+            vals['date_value'] = date
+        res = super(AccountMove, self).create(vals)
         return res
 
     def _get_sale_order_date(self, vals):
@@ -34,9 +37,26 @@ class AccountMove(models.Model):
             return sale_order_date
         for origin in vals['invoice_origin'].split(', '):
             sale_order = self.env['sale.order'].search([('name', '=', origin)], limit=1)
-            if sale_order and (sale_order_date is None or sale_order.date_order > sale_order_date):
-                sale_order_date = sale_order.date_order
+            if sale_order and (sale_order_date is None or sale_order.date_order.date() > sale_order_date):
+                sale_order_date = sale_order.date_order.date()
         return sale_order_date
+
+    def _get_default_value_date(self):
+        for invoice in self:
+            result_date = False
+            if invoice.type == 'out_refund':
+                result_date = invoice.invoice_date
+            else:
+                if invoice.invoice_origin is not False:
+                    for origin in invoice.invoice_origin.split(', '):
+                        sale_order = self.env['sale.order'].search([('name', '=', origin)], limit=1)
+                        if sale_order and (result_date is False or sale_order.date_order.date() > result_date):
+                            result_date = sale_order.date_order.date()
+                    if result_date is False:
+                        result_date = invoice.invoice_date
+                else:
+                    result_date = invoice.invoice_date
+            invoice.date_value = result_date
 
     def _get_invoice_date(self, vals):
         if vals['invoice_date']:
