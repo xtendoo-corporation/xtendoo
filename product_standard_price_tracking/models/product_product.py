374aa7d5a8d750693a8a41aa0fa3ae30e525b038
Manuel Calero <manuelcalerosolis@gmail.com>
2022-03-25 08:13:24 +0100

    tracking

--- product_standard_price_tracking/models/product_product.py
+++ product_standard_price_tracking/models/product_product.py
@@ -16,3 +16,16 @@ class ProductProduct(models.Model):
         In FIFO: value of the last unit that left the stock (automatically computed).
         Used to value the product when the purchase cost is not known (e.g. inventory adjustment).
         Used to compute margins on sale orders.""")
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    standard_price = fields.Float(
+        'Cost', company_dependent=True,
+        digits='Product Price',
+        groups="base.group_user",
+        track_visibility="always",
+        help="""In Standard Price & AVCO: value of the product (automatically computed in AVCO).
+        In FIFO: value of the last unit that left the stock (automatically computed).
+        Used to value the product when the purchase cost is not known (e.g. inventory adjustment).
+        Used to compute margins on sale orders.""")
