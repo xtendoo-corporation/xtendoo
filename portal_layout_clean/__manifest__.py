fe1f82e0a7d320326c4537b5480ab1fc88d9c35d
dani <dani.xtendoo@gmail.com>
2022-04-18 13:48:32 +0200

    [ADD] portal_layout_clean

---
+++ portal_layout_clean/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2022 Daniel Domńiguez (<https://xtendoo.es>).
+
+{
+    "name": "Portal web layout clean",
+    "summary": "Portal web layout clean",
+    "version": "15.0.1.0.0",
+    "author": "Daniel Dominguez -Xtendoo ",
+    "license": "AGPL-3",
+    "website": "https://xtendoo.es",
+    "category": "Tools",
+    "depends": ["web"],
+    "data": [
+        "views/template.xml",
+    ],
+    "installable": True,
+}
