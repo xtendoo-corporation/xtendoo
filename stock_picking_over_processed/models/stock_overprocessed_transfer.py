c12043e87cc26da9d79ba5915607d108bd28adc8
Manuel Calero <manuelcalerosolis@gmail.com>
2022-06-14 13:34:27 +0200

    overprocesed messages more than one product

--- stock_picking_over_processed/models/stock_overprocessed_transfer.py
+++ stock_picking_over_processed/models/stock_overprocessed_transfer.py
@@ -7,6 +7,14 @@ from odoo import _, models, api
 class StockOverProcessedTransfer(models.TransientModel):
     _inherit = "stock.overprocessed.transfer"
 
+
+    def _compute_overprocessed_product_name(self):
+        for wizard in self:
+            wizard.overprocessed_product_name = False
+            if wizard.picking_id:
+                moves = wizard.picking_id._get_overprocessed_stock_moves()
+                wizard.overprocessed_product_name = "; ".join(moves.mapped("product_id.display_name"))
+
     def action_confirm(self):
         self.ensure_one()
         self.picking_id.action_update_sale_order()
