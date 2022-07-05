7057190813244e7819e6cf168e68722d3878c8d0
Manuel Calero <manuelcalerosolis@gmail.com>
2022-07-05 13:42:13 +0200

    mail manual routin fix

--- mail_manual_routing/models/mail_thread.py
+++ mail_manual_routing/models/mail_thread.py
@@ -3,7 +3,6 @@
 import logging
 
 from odoo import api, models, tools
-from odoo.tools.safe_eval import safe_eval
 
 _logger = logging.getLogger(__name__)
 
@@ -28,7 +27,9 @@ class mail_thread(models.AbstractModel):
                 thread_id=thread_id, custom_values=custom_values
             )
         except ValueError:
+            parent_object_id = None
             user_id = self._get_email_to_user(message_dict)
+
             if user_id and user_id.company_id:
                 parent_object_id = self._get_email_from_partner(user_id, message_dict)
 
@@ -46,7 +47,6 @@ class mail_thread(models.AbstractModel):
 
             message_id = self._create_lost_message(**message_dict)
             _logger.warning(u"Message {} can't be routed. Assign it to 'lost messages'".format(message_id))
-            # 1
 
             if user_id:
                 self._create_lost_notification(user_id, parent_object_id)
@@ -127,8 +127,10 @@ class mail_thread(models.AbstractModel):
         email_to = message_dict.get("to")
         if not email_to:
             return None
+
+        email_to = email_to.split('<')[-1].split('>')[0]
         user_id = self.env['res.users'].sudo().search([
-            ('login', '=', email_to)], limit=1)
+            ('email', '=', email_to)], limit=1)
         return user_id
 
     @api.model
@@ -136,39 +138,12 @@ class mail_thread(models.AbstractModel):
         email_from = message_dict.get("email_from")
         if not email_from:
             return None
+
         partner_id = self.env['res.partner'].sudo().search([
             ('email', '=', tools.email_normalize(email_from)), ('company_id', '=', user_id.company_id.id)], limit=1)
+
+        if not partner_id:
+            partner_id = self.env['res.partner'].sudo().search([
+                ('email', '=', tools.email_normalize(email_from))], limit=1)
+
         return partner_id
-
-
-    # @api.model
-    # def _create_notify_message(self, parent_object_id, message_id):
-    #     Config = self.env["ir.config_parameter"].sudo()
-    #     notify_about_lost_messages = safe_eval(Config.get_param("notify_about_lost_messages", "False"))
-    #     if notify_about_lost_messages:
-    #
-    #         notify_lost_user_ids = safe_eval(Config.get_param("notify_lost_user_ids", "[]"))
-    #         notify_lost_channel_ids = safe_eval(Config.get_param("notify_lost_channel_ids", "[]"))
-    #         if notify_lost_user_ids or notify_lost_channel_ids:
-    #             try:
-    #                 self = self.sudo()
-    #                 user_ids = self.env["res.users"].search([("id", "in", notify_lost_user_ids)])
-    #                 partner_ids = user_ids.mapped("partner_id")
-    #                 context = self.env.context.copy()
-    #                 action = self.env.ref("mail_manual_routing.mail_message_action_unattached_open_only_form").id
-    #                 base_url = Config.get_param("web.base.url")
-    #                 db = self.env.cr.dbname
-    #                 url = "{}/web?db={}#id={}&action={}".format(base_url, db, message_id.id, action)
-    #                 context.update({"url": url})
-    #                 template = self.env.ref("mail_manual_routing.lost_message_notification_template")
-    #                 body_html = template.with_context(context)._render_template_qweb(
-    #                     template.body_html, "mail.message", [message_id.id], add_context=context,
-    #                 ).get(message_id.id)
-    #                 subject = template.subject
-    #                 parent_object_id.message_post(body=body_html, subject=subject, partner_ids=partner_ids.ids)
-    #             except Exception as e:
-    #                 _logger.error("Notification by lost message {} is not sent. Reason: {}".format(message_id, e))
-    #
-    #                 print("*" * 80)
-    #                 print("Notification by lost message {} is not sent. Reason: {}".format(message_id, e))
-    #                 print("*" * 80)
