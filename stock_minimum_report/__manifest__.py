69756bcc931ec62650a8452283af4c3111d7b049
dani.xtendoo <dani.xtendoo@gmail.com>
2022-12-30 09:45:58 +0100

    [ADD] stock_minimum_report

---
+++ stock_minimum_report/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2020 Xtendoo - Manuel Calero Solís
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Minimum Report",
+    "description": """
+    Report to show products under minimum
+    """,
+    "version": "12.0.1.0.0",
+    "category": "Stock",
+    "author": "Xtendoo," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "license": "AGPL-3",
+    "depends": ["base", "stock",],
+    "data": ["views/wizards.xml",],
+    "installable": True,
+}
