8acbf42ae6a5c5d7a06415e34edbf94a45c5d9be
Manuel Calero <manuelcalerosolis@gmail.com>
2023-03-15 11:10:06 +0100

    new module xtendoo sale only company

---
+++ xtendoo_sale_only_company/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Manuel Calero <http://www.xtendoo.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Xtendoo Sale Only Company",
+    "summary": "In sale order only company are allowed",
+    "version": "16.0.1.0.0",
+    "author": "Manuel Calero Solís,",
+    "license": "AGPL-3",
+    "website": "https://xtendoo.es",
+    "category": "Tools",
+    "depends": ["sale"],
+    "data": [
+        "view/sale_order_views.xml",
+    ],
+    "installable": True,
+}
