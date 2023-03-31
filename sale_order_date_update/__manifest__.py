b419f07df53ee20d84daef45afa5fcb3db03e72d
Camilx03 <camilojrjbeas@gmail.com>
2023-03-31 10:16:38 +0200

    migration sale_order_date_update to 14.0

---
+++ sale_order_date_update/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2020 Manuel Calero Solis - Xtendoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Order Date Update",
+    "summary": "In a sale order the date can be update",
+    "version": "14.0.1.0.0",
+    "category": "Sale",
+    "website": "https://xtendoo.es",
+    "author": "ddl",
+    "license": "AGPL-3",
+    "depends": ["sale_management",],
+}
