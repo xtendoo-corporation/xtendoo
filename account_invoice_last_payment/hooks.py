42f8757eff6d63916157eb11f1245f75dd2a893e
Manuel Calero <manuelcalerosolis@gmail.com>
2023-04-05 09:11:07 +0200

    clean prints

--- account_invoice_last_payment/hooks.py
+++ account_invoice_last_payment/hooks.py
@@ -8,11 +8,6 @@ def post_init_hook(cr, pool):
     """
     Fetches all invoice and recomputed invoice
     """
-
-    print("*"*80)
-    print("post_init_hook")
-    print("*"*80)
-
     env = Environment(cr, SUPERUSER_ID, {})
     invoices = env["account.move"].search([])
     invoices._compute_amount()
