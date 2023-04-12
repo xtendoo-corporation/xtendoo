fc4a4740547435f6c03d1733e3b5381ffbf3d45d
Camilx03 <camilojrjbeas@gmail.com>
2023-04-12 14:37:44 +0200

    migration to 14.0

---
+++ product_pricelist_margin_base/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Product Pricelist Margin Base",
+    "version": "14.0.1.0.0",
+    "category": "Sales Management",
+    "author": "Manuel Calero, Javier Lagares, Camilo",
+    "website": "https://github.com/xtendoo-corporation/xtendoo",
+    "license": "AGPL-3",
+    "depends": ["product",],
+    "installable": True,
+}
