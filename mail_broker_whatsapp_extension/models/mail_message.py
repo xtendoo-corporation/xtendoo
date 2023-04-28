e37522991258d4e77e201cedd924ca77f712fe4e
Camilx03 <camilojrjbeas@gmail.com>
2023-04-28 08:58:29 +0200

    field state in mail_broker_whatsapp_extension

--- mail_broker_whatsapp_extension/models/mail_message.py
+++ mail_broker_whatsapp_extension/models/mail_message.py
@@ -5,34 +5,17 @@ class MailMessageState(models.Model):
     _inherit = 'mail.message'
 
     state = fields.Selection(
-        [('sent', 'Sent'),
-         ('received', 'Received'),
-         ('exception', 'Exception')],
+        [('sent', 'sent'),
+         ('received', 'received'),
+         ('exception', 'exception')],
         string='State',
         compute='_compute_message_state',
-        default='exception',
     )
 
     @api.depends('notification_ids')
     def _compute_message_state(self):
-        self.state = 'exception'
         for message in self:
-
-            print("*"*80)
-            print("message", message)
-            print("*"*80)
-
             notifications = message.broker_notification_ids
-            if not notifications:
-                print("=" * 80)
-                print("No hay notificaciones")
-                print("=" * 80)
-
-            for notification in notifications:
-                print("=" * 80)
-                print("notification_state", notification.state)
-                print("=" * 80)
-
             if not notifications:
                 message.state = 'exception'
             elif all(notification.state == 'sent' for notification in notifications):
@@ -41,4 +24,3 @@ class MailMessageState(models.Model):
                 message.state = 'received'
             else:
                 message.state = 'exception'
-
