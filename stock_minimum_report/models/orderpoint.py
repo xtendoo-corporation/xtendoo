f9d74fe6a4b830c8104a340b6dfdc6d875841075
Dani <dani.xtendoo@gmail.com>
2023-09-05 11:42:09 +0200

    [ADD] stock_minimum_report

---
+++ stock_minimum_report/models/orderpoint.py
@@ -0,0 +1,22 @@
+from odoo import _, api, fields, models
+
+
+class Orderpoint(models.Model):
+    _inherit = "stock.warehouse.orderpoint"
+
+    qty_available = fields.Float(
+        related="product_id.qty_available",
+    )
+    under_minimum = fields.Boolean(
+        compute="_compute_under_minimum",
+        string="Under Minimum",
+        store=True,
+        help="This value is true if quantity available is under minimum quantity.",
+    )
+    warehouse_name = fields.Char(
+        related="warehouse_id.name"
+    )
+
+    def _compute_under_minimum(self):
+        for record in self:
+            record.under_minimum = (record.qty_available - record.product_min_qty) < 0
