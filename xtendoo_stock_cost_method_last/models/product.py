b53e02f9d1036e0d457715bc984c44d11897b6b7
Dani <dani.xtendoo@gmail.com>
2023-09-14 10:58:40 +0200

    sync

--- xtendoo_stock_cost_method_last/models/product.py
+++ xtendoo_stock_cost_method_last/models/product.py
@@ -8,4 +8,14 @@ from odoo import fields, models
 class ProductCategory(models.Model):
     _inherit = "product.category"
 
-#    property_cost_method = fields.Selection(selection_add=[("last", "Last Price")])
+    property_cost_method = fields.Selection([
+        ('standard', 'Standard Price'),
+        ('fifo', 'First In First Out (FIFO)'),
+        ('average', 'Average Cost (AVCO)'),
+        ("last", "Last Price")], string="Costing Method",
+        company_dependent=True, copy=True, required=True,
+        help="""Standard Price: The products are valued at their standard cost defined on the product.
+            Average Cost (AVCO): The products are valued at weighted average cost.
+            First In First Out (FIFO): The products are valued supposing those that enter the company first will also leave it first.
+            """)
+
