07340914148b015df80c9ac8f569295cce7138b5
ArKloK <ccamachobanez@gmail.com>
2023-10-25 14:14:47 +0200

    New module partner contact mobile

---
+++ partner_contact_mobile/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2015 Tecnativa - Antonio Espinosa
+# Copyright 2015 Tecnativa - Jairo Llopis
+# Copyright 2017 Tecnativa - David Vidal
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Partners Capital",
+    "version": "16.0.0.1.1",
+    "category": "Customer Relationship Management",
+    "license": "AGPL-3",
+    "author": "Antiun Ingeniería S.L., "
+    "Tecnativa, "
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/partner-contact",
+    "depends": ["contacts"],
+    "data": [
+        "views/partner_contact_mobile.xml",
+    ],
+    "installable": True,
+    "maintainers": ["EmilioPascual"],
+}
---
+++ partner_contact_mobile/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module allows the customer to see the field mobile of the model
+res_partner in the tree view of contacts.
