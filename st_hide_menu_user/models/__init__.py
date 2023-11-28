5e6cb58241b8d4671bc1e1388641a47c1e2abdef
Manuel Calero <manuelcalerosolis@gmail.com>
2023-11-28 10:42:59 +0100

    st hide menu

---
+++ st_hide_menu_user/models/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Grupo ADAP S.A.
+#
+#    Copyright (C) 2012-TODAY Grupo ADAP S.A.(<https://www.grupoadap.com>)
+#    Author: Grupo ADAP S.A. Solutions Tools(<https://www.grupoadap.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+
+from . import res_user
