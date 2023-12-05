27c4d529b7cbea5ee9fb6db8029cb01f381c7979
ArKloK <ccamachobanez@gmail.com>
2023-12-05 12:32:35 +0100

    Migration to 15.0

---
+++ sale_order_only_company/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2020 Xtendoo - Manuel Calero Solís
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Order Only Company",
+    "description": """
+    Sale order only company
+    """,
+    "version": "15.0.1.0.0",
+    "category": "Sale",
+    "author": "Xtendoo, Dani Domínguez",
+    "website": "https://xtendoo.esw",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+        "sale",
+    ],
+    "data": [
+        "views/sale_order.xml",
+    ],
+    "installable": True,
+}
+
+
