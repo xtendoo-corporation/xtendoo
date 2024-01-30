4dabc1eb7ba299bbbc2d4635f6300160c6c60970
manuelcalerosolis <manuelcalerosolis@gmail.com>
2024-01-30 13:06:15 +0100

    new module account no sale description

---
+++ account_no_sale_description/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Account No Sale Description',
+    'version': '16.0.1',
+    'category': 'Accounting',
+    'summary': 'Remove product reference from invoice line description',
+    'depends': [
+        'account'
+    ],
+    'installable': True,
+}
