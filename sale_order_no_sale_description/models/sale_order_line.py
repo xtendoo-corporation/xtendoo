caa474610bba946d0331a7c13f2ab1203bf249c4
ArKloK <ccamachobanez@gmail.com>
2024-01-30 13:54:29 +0100

    Created a new module sale_order_no_sale_description

---
+++ sale_order_no_sale_description/models/sale_order_line.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, api
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    @api.depends('product_id')
+    def _compute_name(self):
+        print("xtendoo sale_order_no_sale_description")
+        super()._compute_name()
+        for line in self:
+            if line.product_id:
+                lang = line.order_id.partner_id.lang
+                line.name = line.product_id.with_context(lang=lang).name or ''
