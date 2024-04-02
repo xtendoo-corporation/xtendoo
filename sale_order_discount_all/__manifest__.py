75f770e0613c6094278b961b26d39cb4c649a797
Gokac9 <theabraham9@gmail.com>
2024-04-02 10:36:43 +0200

    insert a button in sale_order to discount all products 100%

---
+++ sale_order_discount_all/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Enteza - Visueña de material plegable S.L.",
+    "summary": "Adds new button to set all discounts to 100%",
+    "version": "15.0.1.0.0",
+    "category": "Sales",
+    "author": "Manuel Calero, Abraham Carrasco, Xtendoo",
+    "license": "LGPL-3",
+    "application": True,
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/sale_order_line_view.xml",
+    ],
+    "installable": True,
+}
