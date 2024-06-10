3f555395facdb4efcf60e47c557770913defba57
Gokac9 <theabraham9@gmail.com>
2024-06-10 12:24:53 +0200

    Update module security, users permisions

--- firebase_notifications/__manifest__.py
+++ firebase_notifications/__manifest__.py
@@ -10,8 +10,9 @@
         'mail',
     ],
     'data': [
+        'data/groups.xml',
+        'views/res_users_groups_notifications.xml',
         'views/firebase_notification_view.xml',
-        'views/firebase_json_upload_view.xml',
         'views/res_config_view_notifications.xml',
         'data/cron.xml',
         'security/ir.model.access.csv',
--- firebase_notifications/models/__init__.py
+++ firebase_notifications/models/__init__.py
@@ -2,3 +2,4 @@ from . import firebase_notification
 from . import firebase_notificacion_tag
 from . import firebase_data
 from . import res_config_settings
+from . import res_users
