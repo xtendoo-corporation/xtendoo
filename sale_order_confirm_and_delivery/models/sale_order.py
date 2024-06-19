66b4b34246273372613a7eac687d29de7b01212c
manuelcalerosolis <manuelcalerosolis@gmail.com>
2024-06-19 23:26:14 +0200

    clean code

--- sale_order_confirm_and_delivery/models/sale_order.py
+++ sale_order_confirm_and_delivery/models/sale_order.py
@@ -8,27 +8,24 @@ class SaleOrder(models.Model):
     # y para confirmar y facturar un pedido en un solo clic.
     # Pero la entrega y la factura hay que confirmarlas manualmente.
     def action_confirm_and_deliver(self):
-        print("*" * 80, "print 1")
+        print("*"*80)
+        print("action_confirm_and_deliver")
+        print("*"*80)
+
         self.action_confirm()
         print("*" * 80 , "despues action_confirm")
-        for order in self:
-            print("*" * 80, "for order in self")
-            if order.state in ['sale', 'draft'] and order.picking_ids:
-                print("*" * 80 , "order.state in ['sale', 'draft']")
-                for picking in order.picking_ids:
-                    print("*" * 80, "picking in order.picking_ids")
-                    picking.action_assign()
-                    # if picking.state == 'assigned':
-                    if picking.state in ['assigned', 'confirmed']:
-                        print("*" * 80, "picking.state in ['assigned', 'confirmed']:")
-                        if not picking.move_line_ids:
-                            print("No move lines for this picking")
-                        for move_line in picking.move_line_ids:
-                            print("*"*80)
-                            print(move_line.product_uom_qty, "cantidad")
-                            print(move_line.quantity_done, "cantidad hecha")
-                            move_line.quantity_done = move_line.product_uom_qty
-                        picking.button_validate()
+        for order in self.filtered(lambda o: o.state in ['sale', 'draft']):
+
+            print("*"*80)
+            print("order.state", order.state)
+            print("*"*80)
+
+            for picking in order.picking_ids.filtered(lambda p: p.state not in ['assigned', 'confirmed']):
+                picking.action_assign()
+                for move in picking.move_ids_without_package:
+                    move._action_assign()
+                    move._set_quantity_done(move.product_uom_qty)
+                picking.with_context(skip_immediate=True, skip_sms=True).button_validate()
 
     def action_confirm_and_invoice(self):
         self.action_confirm_and_deliver()
