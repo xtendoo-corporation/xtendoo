16be9b653d9cf30152587a546a2fad09d72b6da5
dani <dani.xtendoo@gmail.com>
2024-06-19 11:09:16 +0200

    [MIG] Purchase order cost price

---
+++ purchase_order_cost_price/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2020 Manuel Calero Solis - Xtendoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Purchase Order Cost Price",
+    "summary": "If the relationship with the supplier does not exist or its price is 0.00, use the cost price",
+    "version": "15.0.1.0.0",
+    "category": "Purchases",
+    "website": "xtendoo.es",
+    "author": "ddl",
+    "license": "AGPL-3",
+    "depends": ["purchase",],
+    "data": [],
+}
