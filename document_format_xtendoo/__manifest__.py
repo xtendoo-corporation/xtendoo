ce984315913b79b2c7ed77a001380409a7d26e05
GonzalezJimenezSalvador <salvador.gon.jim@gmail.com>
2024-09-12 08:42:43 +0200

    migration 15 -> 16

---
+++ document_format_xtendoo/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "document_format_xtendoo",
+    "summary": """Formatos de documentos Xtendoo""",
+    "version": "16.0.1.0.0",
+    "description": """Formatos de documentos Xtendoo""",
+    "author": "DDL",
+    "company": "Xtendoo",
+    "website": "http://www.xtendoo.es",
+    "category": "Extra Tools",
+    "depends": ["document_format_clean"],
+    "license": "AGPL-3",
+    "data": [
+        "views/layout/external_layout_clean.xml",
+        "views/invoice/invoice_document.xml",
+    ],
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
+}
