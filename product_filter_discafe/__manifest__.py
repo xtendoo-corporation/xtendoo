a3013a2152bfab17eb37779f32e177ca85cdb99c
GonzalezJimenezSalvador <salvador.gon.jim@gmail.com>
2024-10-09 13:09:43 +0200

    discafe custom product filter

---
+++ product_filter_discafe/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Product Filter Discafe',
+    'summary': 'Filter products by quantity',
+    'version': '1.1',
+    'website': 'https://xtendoo.es/',
+    'depends': ['stock', 'product_expiry'],
+    'license': 'LGPL-3',
+    'category': 'Warehouse',
+    'data': [
+        'views/stock_production_lot_views_discafe.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
