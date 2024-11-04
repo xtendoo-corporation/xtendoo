98462ca92a2402d87dc1f85448e019bdb377980b
manuelcalerosolis <manuelcalerosolis@gmail.com>
2024-11-05 00:44:14 +0100

    menu_finance_entries_accounting_ledgers

---
+++ menu_finance_entries_accounting_ledgers/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "Menu Finance Entries Accounting Ledgers",
+    'version': '16.0.1.0.0',
+    'summary': """Menu Finance Entries Accounting Ledgers""",
+    'description': """Menu Finance Entries Accounting Ledgers""",
+    'author': "Xtendoo",
+    'company': "Xtendoo",
+    'maintainer': 'Xtendoo',
+    'website': "https://xtendoo.es",
+    'category': 'Tools',
+    'depends': [
+        'account',
+    ],
+    'license': 'AGPL-3',
+    'data': [
+        'views/account_menuitem.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
