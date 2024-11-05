2a712a023480093d21ccb8b262c75ead82c42b5e
GonzalezJimenezSalvador <salvador.gon.jim@gmail.com>
2024-11-05 11:35:00 +0100

    show ledger by partner

--- menu_finance_entries_accounting_ledgers/__manifest__.py
+++ menu_finance_entries_accounting_ledgers/__manifest__.py
@@ -3,7 +3,7 @@
     'version': '16.0.1.0.0',
     'summary': """Menu Finance Entries Accounting Ledgers""",
     'description': """Menu Finance Entries Accounting Ledgers""",
-    'author': "Xtendoo",
+    'author': "Salvador Gonzalez (Xtendoo)",
     'company': "Xtendoo",
     'maintainer': 'Xtendoo',
     'website': "https://xtendoo.es",
