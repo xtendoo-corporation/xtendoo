fb2b50b575da0a4032de2d15ddddf7c7771adef8
Gokac9 <theabraham9@gmail.com>
2024-11-14 12:26:09 +0100

    add module product_pricelist_item_extended_view

---
+++ product_pricelist_item_extended_view/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Product Pricelist Item Extended View",
+    "version": "17.0.1.0.1",
+    "category": "Sales",
+    "author": "Xtendoo",
+    "license": "AGPL-3",
+    "depends": [
+       "product",
+    ],
+    "data": [
+        "views/product_pricelist_item_view.xml",
+    ],
+    "installable": True,
+}
