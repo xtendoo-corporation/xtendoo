69b24033680dfa8f809fe351c395af483d128d08
Gokac9 <theabraham9@gmail.com>
2024-11-18 14:29:16 +0100

    add xtendoo_product_label

--- garazd_product_label/models/res_config_settings.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    replace_standard_wizard = fields.Boolean(config_parameter='garazd_product_label.replace_standard_wizard')
---
+++ xtendoo_product_label/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': 'Xtendoo Custom Product Labels',
+    'version': '18.0.1.0.1',
+    'category': 'Extra Tools',
+    'author': 'Abraham Carrasco (Xtendoo)',
+    'website': '',
+    'license': 'LGPL-3',
+    'summary': 'Print custom product labels',
+    'images': [],
+    'depends': [
+        'product',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/product_data.xml',
+        'data/print_label_type_data.xml',
+        'data/ir_filters_data.xml',
+        'report/product_label_reports.xml',
+        'report/product_label_templates.xml',
+        'wizard/print_product_label_views.xml',
+        'views/res_config_settings_views.xml',
+    ],
+    'demo': [
+    ],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+}
