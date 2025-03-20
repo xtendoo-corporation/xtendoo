792b7ef4bcd0a8ef12e78a69b1997c302838a04e
manuelcalerosolis <manuelcalerosolis@gmail.com>
2025-03-20 12:40:12 +0100

    xtendoo backup url

---
+++ xtendoo_backup_url/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Xtendoo Backup Url',
+    'version': '18.0.1.0.0',
+    'category': 'Tools',
+    "author": "Manuel Calero, Xtendoo",
+    'description': "Module to get database backup from url",
+    'depends': [
+        'base',
+        'web',
+    ],
+    'auto_install': False,
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
