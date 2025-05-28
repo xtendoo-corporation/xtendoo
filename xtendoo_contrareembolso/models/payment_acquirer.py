fd680d08357cddb6812b95f9c255a2ec5fc28138
Guillermo Barcena <barcenalopezguillermo@gmail.com>
2025-05-28 07:01:36 +0200

    modulo innecesario eliminado

--- xtendoo_contrareembolso/models/payment_acquirer.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class PaymentProviderCOD(models.Model):
-    _inherit = 'payment.provider'
-
-    def _get_provider_types(self):
-        types = super()._get_provider_types()
-        types.append(('cod', 'Contrareembolso'))
-        return types
