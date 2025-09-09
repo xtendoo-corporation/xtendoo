4f4f3c0d60e63dc6a99954c0cbba2f31f7f54aaf
dani <dani.xtendoo@gmail.com>
2025-09-09 12:31:11 +0200

    [MIG]project task kind to 15.0

---
+++ project_task_kind/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 Xtendoo
+
+{
+    "name": "Project task kind",
+    "summary": """
+        Project task kind""",
+    "version": "15.0",
+    "depends": ["project"],
+    "maintainers": ["dariocruzmauro"],
+    "author": "Xtendoo",
+    "license": "AGPL-3",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/project_task_kind_view.xml",
+        "views/project_task_kind_menu.xml"
+            ],
+    "installable": True,
+    "auto_install": True,
+}
+
