2ee3136b1ee77fb1c2997ee7857eb2af256f78a4
manuelcalerosolis <manuelcalerosolis@gmail.com>
2025-10-03 10:32:09 +0200

    new module xtendoo only

---
+++ xtendoo_sale_filter_delivery_address/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 Salvador Aramis González Jiménez(https://xtendoo.es)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Globaldamac Sale Filter Delivery Address",
+    "version": "17.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Abraham Carrasco Molina, Xtendoo",
+    "category": "Website",
+    "depends":
+        [
+            "sale",
+        ],
+    "data":
+        [
+            "views/sale_view_order_form_inherit.xml",
+        ],
+    'installable': True,
+    'active': False,
+}
