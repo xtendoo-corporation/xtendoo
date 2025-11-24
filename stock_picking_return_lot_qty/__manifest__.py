f2682a1b7fdf98aae22092d8aaef5cb22b49e8d5
manuelcalerosolis <manuelcalerosolis@gmail.com>
2025-11-24 23:34:01 +0100

    sync

--- stock_picking_return_lot_qty/__manifest__.py
+++ stock_picking_return_lot_qty/__manifest__.py
@@ -6,11 +6,13 @@
     "summary": "Return  to delivered quantity",
     "version": "18.0.1.0",
     "license": "AGPL-3",
-    "author": "Xtendoo,Dani Dominguez",
-    "website": "https://github.com/OCA/stock-logistics-workflow/",
-    "depends": ["stock"],
+    "author": "Xtendoo, Manuel Calero",
+    "website": "https://xtendoo.es",
+    "depends": [
+        "stock",
+    ],
     "data": [
         "views/views.xml",
-        "wizard/stock_picking_return_views.xml"
-             ],
+        "wizard/stock_picking_return_views.xml",
+    ],
 }
