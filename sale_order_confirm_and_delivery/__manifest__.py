a614857dd644ae4865bbd09f233c4034d73b8c0f
Guillermo Bárcena López <barcenalopezguillermo@gmail.com>
2025-12-15 10:50:14 +0100

    sync

---
+++ sale_order_confirm_and_delivery/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Sale order confirm and delivery",
+    "version": "18.0.1.0.0",
+    "author": "Daniel Domínguez (https://xtendoo.es)",
+    "category": "Sale order",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+        "stock",
+    ],
+    "data": [
+        "views/sale_view_order_form.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
