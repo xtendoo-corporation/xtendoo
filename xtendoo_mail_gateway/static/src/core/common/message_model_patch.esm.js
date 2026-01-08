6e6ff20571adcb54e71acb7c8501eb9d50eb1fca
Gokac9 <theabraham9@gmail.com>
2026-01-08 14:40:44 +0100

    update code

--- xtendoo_mail_gateway/static/src/core/common/message_model_patch.esm.js
+++ xtendoo_mail_gateway/static/src/core/common/message_model_patch.esm.js
@@ -1,7 +1,7 @@
 import {Message} from "@mail/core/common/message_model";
 import {patch} from "@web/core/utils/patch";
 import {url} from "@web/core/utils/urls";
-import {stateToUrl} from "@web/core/browser/router";
+import {router} from "@web/core/browser/router";
 
 patch(Message.prototype, {
     setup() {
@@ -9,11 +9,11 @@ patch(Message.prototype, {
         this.gateway_thread_data = {};
     },
     get resUrl() {
-        if (!this.gateway_thread_data) {
+        if (!this.gateway_thread_data || !this.gateway_thread_data.model) {
             return super.resUrl;
         }
         return url(
-            stateToUrl({
+            router.stateToUrl({
                 model: this.gateway_thread_data.model,
                 resId: this.gateway_thread_data.id,
             })
