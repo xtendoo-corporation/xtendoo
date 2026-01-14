9e233e1c1704af8f91a1d0806bbc6ecd5aadaefb
Gokac9 <theabraham9@gmail.com>
2026-01-14 14:48:20 +0100

    sync updates to send pos ticket from whatsapp more logs v16 send only template

--- xtendoo_whatsapp_pos_ticket/models/whatsapp_pending_confirmation.py
+++ xtendoo_whatsapp_pos_ticket/models/whatsapp_pending_confirmation.py
@@ -12,17 +12,14 @@ class WhatsappPendingConfirmation(models.Model):
 
     def process_confirmation_response(self, message_data):
         res = super().process_confirmation_response(message_data)
-        # Si la confirmación es para un pedido POS y la respuesta es 'si_ticket', enviar el ticket
+        # Si la confirmación es para un pedido POS y la respuesta es afirmativa, enviar el ticket
         if self.res_model == 'pos.order' and self.state == 'confirmed':
-            # Buscar el pedido POS
             pos_order = self.env['pos.order'].browse(self.res_id)
-            if pos_order.exists():
-                # Obtener el HTML del ticket (puedes guardar el HTML en un campo, attachment, o generarlo)
-                ticket_html = getattr(pos_order, 'last_ticket_html', None)
-                if not ticket_html:
-                    _logger.warning(f"[WhatsApp POS] No se encontró el HTML del ticket para el pedido {pos_order.name}")
-                else:
-                    _logger.info(f"[WhatsApp POS] Enviando ticket PDF por WhatsApp tras confirmación interactiva para el pedido {pos_order.name}")
-                    pos_order.send_whatsapp_ticket_html(pos_order.id, True, ticket_html)
+            ticket_html = self.ticket_html
+            if not ticket_html:
+                _logger.warning(f"[WhatsApp POS] No se encontró el HTML del ticket para el pedido {pos_order.name}")
+            else:
+                _logger.info(f"[WhatsApp POS] Enviando ticket PDF por WhatsApp tras confirmación interactiva para el pedido {pos_order.name}")
+                pos_order.send_whatsapp_ticket_pdf(ticket_html)
         return res
 
