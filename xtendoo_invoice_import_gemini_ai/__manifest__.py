9def2d4374b08a0b017ad58e67253f5b26ca15ab
manuelcalerosolis <manuelcalerosolis@gmail.com>
2026-01-14 13:13:24 +0100

    sync

--- xtendoo_invoice_import_gemini_ai/__manifest__.py
+++ xtendoo_invoice_import_gemini_ai/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Invoice Import Gemini AI",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Accounting",
     "author": "Xtendoo",
     "website": "https://xtendoo.es",
