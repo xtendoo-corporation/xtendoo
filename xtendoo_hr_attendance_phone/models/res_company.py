ca71def48e899dc089ad5fb0acb0cc8ef925f55c
EndikaPM <endika.perez.mas@gmail.com>
2026-01-21 09:23:16 +0100

    add xtendoo_hr_attendance_phone module

---
+++ xtendoo_hr_attendance_phone/models/res_company.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    # Extender el campo attendance_kiosk_mode para agregar las opciones de teléfono
+    attendance_kiosk_mode = fields.Selection(
+        selection_add=[
+            ('phone', 'Teléfono + PIN'),
+            ('phone_only', 'Solo Teléfono')
+        ],
+        ondelete={
+            'phone': 'set default',
+            'phone_only': 'set default'
+        }
+    )
+
