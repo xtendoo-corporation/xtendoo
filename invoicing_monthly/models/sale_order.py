c58d4a03b7d2842f44be8c0454dd450c38bf0d5c
Gokac9 <theabraham9@gmail.com>
2026-05-29 08:19:38 +0200

    fix field date

--- invoicing_monthly/models/sale_order.py
+++ invoicing_monthly/models/sale_order.py
@@ -9,7 +9,7 @@ from datetime import datetime, timedelta
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    def _create_invoices(self, grouped=False, final=False):
+    def _create_invoices(self, grouped=False, final=False, date=None):
         for order in self:
             if order.partner_id.monthly_invoicing:
                 date_order = format(order.date_order.month) + format(order.date_order.year)
@@ -20,4 +20,4 @@ class SaleOrder(models.Model):
                         %
                         (order.partner_id.name, order.name)
                     )
-        return super(SaleOrder, self)._create_invoices(grouped, final)
+        return super(SaleOrder, self)._create_invoices(grouped=grouped, final=final, date=date)
