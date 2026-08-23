59375ace566f82716167d7bccbdae16a0652d015
manuelcalerosolis <manuelcalerosolis@gmail.com>
2026-08-23 19:57:09 +0200

    [MIG] xtendoo_sale_order_archive, no_pagination_lines: Migration to 16.0 (dji)

---
+++ no_pagination_lines/models/sale.py
@@ -0,0 +1,21 @@
+# Copyright 2020 Manuel Calero Solis - Xtendoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import _, api, fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    total_invoiced = fields.Monetary(
+        string="Total Invoiced",
+        readonly=True,
+        compute="_compute_total_invoiced",
+        help="Sale order total invoiced",
+    )
+
+    def _compute_total_invoiced(self):
+        for record in self:
+            total_invoice = 0.0
+            for invoice in record.invoice_ids:
+                total_invoice += invoice.amount_total_signed
+            record.total_invoiced = total_invoice
