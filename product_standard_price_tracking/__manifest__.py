08f9969ca3034b1b5928403dbb34523e418f4534
manuelcalerosolis <manuelcalerosolis@gmail.com>
2026-08-25 21:18:28 +0200

    [MIG] product_standard_price_tracking: Migration to 18.0

--- product_standard_price_tracking/__manifest__.py
+++ product_standard_price_tracking/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Product Standard Price Tracking",
     "summary": "Add tracking to partner standard price",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Purchase",
     "author": "Xtendoo, Odoo Community Association (OCA), Camilo",
     "license": "AGPL-3",
