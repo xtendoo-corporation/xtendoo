3fd7cfd9be560229736ce681bdccc4a899da652d
manuelcalerosolis <manuelcalerosolis@gmail.com>
2026-08-25 21:18:28 +0200

    [MIG] xtendoo_sale_order_archive: Migration to 18.0

--- xtendoo_sale_order_archive/__manifest__.py
+++ xtendoo_sale_order_archive/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "Xtendoo, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/sale-workflow",
     "category": "Sales",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["sale"],
     "data": ["views/sale_order.xml"],
